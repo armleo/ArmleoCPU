dddcc9b7b1daf7483e6b464cfb3ca40606a14a15
armleo <aa-c-aa@mail.ru>
2020-03-14 16:41:19 +0400

    Cache/ Testbench test cases list created

--- testbenches/cache/armleocpu_cache_tb.sv
+++ testbenches/cache/armleocpu_cache_tb.sv
@@ -12,25 +12,53 @@ initial begin
 	$finish;
 end
 
-
-
 /*
-task read();
-input cached;
-input vtag, set, way, offset;
-input load_type;
 
-@(negedge clk);
+PTW Megapage Access fault
+PTW Page access fault
+Cache memory access fault
 
-@(posedge clk);
-// assertions and waits
+PTW Megapage pagefault
+PTW Page pagefault
+Cache memory pagefault for each case (read, write, execute, access, dirty, user)
 
-endtask
+For two independent lanes
+	PTW Megapage Access fault
+	PTW Page access fault
+	Cache memory access fault
 
-initial begin
+	PTW Megapage pagefault
+	PTW Page pagefault
+	Cache memory pagefault for each case (read, write, execute, access, dirty, user)
+	For each csr_satp_mode = 0 and csr_satp_mode = 1
+		For address[33:32] = 0 and address[33:32] != 0
+			For each load type and store type combination
+				Bypassed load
+				Bypassed load after load
+				Bypassed store
+				Bypassed load after store
+				Bypassed store after store
 
-end
+				Cached load
+				Cached load after load
+				Cached store
+				Cached load after store
+				Cached store after store
+		For each unknown type for load
+			Bypassed load
+			Cached load
+		For each unknown type for store
+			Bypassed store
+			Cached store
+		For each missaligned address for each store case
+			Bypassed store
+			Cached store
+		For each missaligned address for each load case
+			Bypassed load
+			Cached load
+	Flush
 
+Generate random access pattern using GLFSR, check for validity
 */
 
 
