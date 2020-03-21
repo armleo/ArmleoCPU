328416c2660b65dc7d65b66cb8e7307ec456910b
armleo <aa-c-aa@mail.ru>
2020-03-21 15:52:00 +0400

    Made build script for quartus as include

---
+++ testbenches/QuartusBuild.mk
@@ -0,0 +1,65 @@
+
+all: smart.log $(PROJECT).asm.rpt $(PROJECT).sta.rpt 
+
+clean:
+	rm -rf $(PROJECT).sdc *.rpt *.chg smart.log *.htm *.eqn *.pin *.sof *.pof *.qpf *.qsf *.qws *.summary *.smsg *.jdi *.jdi db incremental_db simulation
+
+map: smart.log $(PROJECT).map.rpt
+fit: smart.log $(PROJECT).fit.rpt
+asm: smart.log $(PROJECT).asm.rpt
+sta: smart.log $(PROJECT).sta.rpt
+smart: smart.log
+
+###################################################################
+# Executable Configuration
+###################################################################
+
+MAP_ARGS = --read_settings_files=on $(addprefix --source=,$(SRCS))
+
+FIT_ARGS = --part=$(PART) --read_settings_files=on
+ASM_ARGS =
+STA_ARGS =
+
+###################################################################
+# Target implementations
+###################################################################
+
+$(PROJECT).sdc: timing.sdc
+	cp timing.sdc $(PROJECT).sdc
+
+STAMP = echo done >
+
+$(PROJECT).map.rpt: $(SOURCE_FILES) $(PROJECT).sdc
+	quartus_map $(MAP_ARGS) $(PROJECT)
+
+$(PROJECT).fit.rpt: $(PROJECT).map.rpt
+	quartus_fit $(FIT_ARGS) $(PROJECT)
+
+$(PROJECT).asm.rpt: $(PROJECT).fit.rpt
+	quartus_asm $(ASM_ARGS) $(PROJECT)
+
+$(PROJECT).sta.rpt: $(PROJECT).fit.rpt
+	quartus_sta $(STA_ARGS) $(PROJECT) 
+
+smart.log: $(ASSIGNMENT_FILES)
+	quartus_sh --determine_smart_action $(PROJECT) > smart.log
+	
+
+###################################################################
+# Project initialization
+###################################################################
+
+$(ASSIGNMENT_FILES):
+	quartus_sh --prepare -f $(FAMILY) -t $(TOP_LEVEL_ENTITY) $(PROJECT)
+	-cat qsf_append >> $(PROJECT).qsf
+	-cat $(BOARDFILE) >> $(PROJECT).qsf
+
+###################################################################
+# Programming the device
+###################################################################
+
+program: $(PROJECT).sof
+	quartus_pgm --no_banner --mode=jtag -o "P;$(PROJECT).sof"
+
+program-pof: $(PROJECT).pof
+	quartus_pgm --no_banner --mode=jtag -o "BVP;$(PROJECT).pof"
