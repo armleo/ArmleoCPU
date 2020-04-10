bee7e0440468eca21f2ed16a9704ff004924af6d
armleo <aa-c-aa@mail.ru>
2020-04-10 09:41:43 +0400

    Added verilator linting

--- testbenches/SimulateTemplate.mk
+++ testbenches/SimulateTemplate.mk
@@ -15,12 +15,15 @@ build: $(netlist)
 execute: $(simresult)
 	
 $(simresult): $(netlist) ../clk_gen_template.svh ../sync_clk_gen_template.svh ../SimulateTemplate.mk ../assert.svh Makefile
-	$(vvp) $(netlist) $(vvpparams) > execute_logfile.log
+	$(vvp) $(netlist) $(vvpparams) &> execute_logfile.log
 $(netlist): $(files) Makefile
-	$(iverilog) -Wall -g2005 $(includepaths) -o $(netlist) -D__ICARUS__=1 -DSIMRESULT="\"$(simresult)\"" -DDEBUG $(files) > compile_logfile.log
+	$(iverilog) -Wall -g2005 $(includepaths) -o $(netlist) -D__ICARUS__=1 -DSIMRESULT="\"$(simresult)\"" -DDEBUG $(files) $(tbfiles) &> compile_logfile.log
+lint:
+	-verilator --lint-only -Wall $(includepaths) $(files) -DSIMRESULT="\"$(simresult)\"" > verilator.lint.log 2>&1
 clean:
 	rm -f *.lxt2
 	rm -f *.vcd
 	rm -f $(netlist)
 	rm -f execute_logfile.log
 	rm -f compile_logfile.log
+	rm -f verilator.lint.log
