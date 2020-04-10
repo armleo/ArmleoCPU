a5fa2f0d05e4228321678f17d28ae8165875458e
Armleo <7474152+armleo@users.noreply.github.com>
2020-04-10 11:21:37 +0400

    Reoworked makefiles. TODO: rework for verilog 2005

--- testbenches/SimulateTemplate.mk
+++ testbenches/SimulateTemplate.mk
@@ -5,7 +5,10 @@ iverilog=iverilog
 vvp=vvp
 gtkwave=gtkwave
 includepaths+=../ ../../src/includes/
+
+
 includepathsI=$(addprefix -I,$(includepaths))
+
 view: $(simresult)
 	$(gtkwave) $(simresult)
 
@@ -16,15 +19,10 @@ execute: $(simresult)
 $(simresult): $(netlist) ../clk_gen_template.svh ../sync_clk_gen_template.svh ../SimulateTemplate.mk ../assert.svh Makefile
 	-$(vvp) $(netlist) $(vvpparams) &> execute_logfile.log
 $(netlist): $(files) Makefile
-	-$(iverilog) -Wall -g2005 $(includepathsI) -o $(netlist) -D__ICARUS__=1 -DSIMRESULT="\"$(simresult)\"" -DDEBUG $(files) $(tbfiles) &> compile_logfile.log
+	-$(iverilog) -Wall -g2012 $(includepathsI) -o $(netlist) -D__ICARUS__=1 -DSIMRESULT="\"$(simresult)\"" -DDEBUG $(files) $(tbfiles) &> compile_logfile.log
 lint:
 	-verilator --lint-only -Wall $(includepathsI) $(files) -DSIMRESULT="\"$(simresult)\"" &> verilator.lint.log
 lint-xvlog:
 	-xvlog $(files) $(addprefix -i ,$(includepaths)) &> xvlog.lint.log
 clean:
-	rm -f *.lxt2
-	rm -f *.vcd
-	rm -f $(netlist)
-	rm -f execute_logfile.log
-	rm -f compile_logfile.log
-	rm -f verilator.lint.log
+	rm -rf *.vcd *.lxt2 xvlog* xsim* verilator.lint.log compile_logfile.log execute_logfile.log $(netlist)
