f0c1a908c254c7e8cecf139a12cb03ac7fc3bee6
Armleo <7474152+armleo@users.noreply.github.com>
2020-04-21 10:29:23 +0400

    TLB: Redesign complete and tested

--- src/corevx_tlb.sv
+++ src/corevx_tlb.sv
@@ -26,6 +26,8 @@ parameter ENTRIES_W = 4;
 localparam ENTRIES = 2**ENTRIES_W;
 parameter disable_debug = 0;
 
+`include "corevx_tlb_defs.svh"
+
 /*
 	Address structure from virtual
 	|20-ENTRIES_W bits		|ENTRIES_W bits	| 12 bit	  	|
@@ -85,6 +87,12 @@ mem_1w1r #(
     .writedata(virt_tag_w)
 );
 
+reg accesstag_read;
+wire [7:0] accesstag_readdata;
+reg accesstag_write;
+reg [ENTRIES_W-1:0]accesstag_write_set_index;
+reg [7:0] accesstag_writedata;
+
 mem_1w1r #(
     .ELEMENTS_W(ENTRIES_W),
     .WIDTH(8)
@@ -106,14 +114,16 @@ reg [32-12-ENTRIES_W-1:0]   os_virt_tag;
 reg os_active;
 `endif
 
-assign phys_r      = phys_readdata;
+assign phys_r      = ptag_readdata;
 assign accesstag_r = accesstag_readdata;
-assign hit         = accesstag_readdata[0] && (os_virt_tag == vtag_readdata)
+assign hit         = accesstag_readdata[0] && (os_virt_tag == vtag_readdata);
 
 always @* begin
     ptag_read       = 1'b0;
     ptag_write      = 1'b0;
-
+    vtag_read                       = 1'b0;
+    accesstag_read                  = 1'b0;
+    vtag_write                      = 1'b0;
     accesstag_write                 = 1'b0;
     accesstag_writedata             = accesstag_w;
     accesstag_write_set_index       = set_index_w;
@@ -147,19 +157,22 @@ always @(posedge clk) begin
         if(command == `TLB_CMD_RESOLVE) begin
             `ifdef DEBUG
                 os_active <= 1'b1;
+                if(!disable_debug) begin
+                    $display("[%d][TLB] TLB Resolve virtual_address=0x%X", $time, virtual_address);
+                end
             `endif
             os_virt_tag <= virt_tag;
             // used in output stage for hit calculation
         end else if(command == `TLB_CMD_WRITE) begin
             `ifdef DEBUG
             if(!disable_debug)
-                $display("[%d][TLB %d] TLB Write ", $time, );
+                $display("[%d][TLB] TLB Write virtual_address_w = 0x%X, accesstag_w = 0x%X, phys_w = 0x%X", $time, virtual_address_w, accesstag_w, phys_w);
             `endif
             // nothing in sync
         end else if(command == `TLB_CMD_INVALIDATE) begin
             `ifdef DEBUG
             if(!disable_debug)
-                $display("[%d][TLB %d] TLB Invalidate ", $time, );
+                $display("[%d][TLB] TLB Invalidate invalidate_set_index=0x%X", $time, invalidate_set_index);
             `endif
             // nothing in sync
         end
@@ -167,14 +180,9 @@ always @(posedge clk) begin
         if(!disable_debug)
             if(os_active) begin
                 if(hit) begin
-                    $display("[%d][TLB %d] Resolve complete, hit accesstag_r = 0x%X, os_virt_tag = 0x%x", $time, accesstag_r, os_virt_tag);
+                    $display("[%d][TLB] Resolve complete, hit accesstag_r = 0x%X, os_virt_tag = 0x%x", $time, accesstag_r, os_virt_tag);
                 end else begin
-                    if(!valid[set_index_r])
-                        $display("[%d][TLB %d] Resolve missed because invalid", $time);
-                    else if(!hit)
-                        $display("[%d][TLB %d] Resolve missed because tag is different", $time);
-                    else
-                        $display("[%d][TLB %d] Unknown resolve result [BUG]", $time);
+                        $display("[%d][TLB] Resolve missed", $time);
                 end
             end
 		`endif
@@ -186,16 +194,15 @@ end
 endmodule
 
 
-/*
 module corevx_tlb(
     input clk,
     input rst_n,
     
-    input [19:0]        virtual_address,
     // commands
     input [1:0]         command,
 
     // read port
+    input [19:0]        virtual_address,
     output  reg         hit,
     output  reg [7:0]   accesstag_r,
     output  reg [21:0]  phys_r,
@@ -203,7 +210,10 @@ module corevx_tlb(
     // write port
     input       [19:0]  virtual_address_w,
     input       [7:0]   accesstag_w,
-    input       [21:0]  phys_w
+    input       [21:0]  phys_w,
+
+    // invalidate port
+    input       [ENTRIES_W-1:0] invalidate_set_index
 );
 
 parameter  ENTRIES_W = 4;
@@ -211,18 +221,81 @@ parameter  ENTRIES_W = 4;
 parameter  WAYS_W = 2;
 localparam WAYS = 2**WAYS_W;
 
+reg [WAYS_W-1:0] victim_way;
+
+always @(posedge clk) begin
+    if(!rst_n) begin
+        victim_way <= 0;
+    end else begin
+        if(command == `TLB_CMD_WRITE)
+            victim_way <= victim_way + 1;
+    end
+end
+
 /*
 for resolve request resolve for all tlb  ways
 for write, write to tlb[victim_way]
 for invalidate, write 0 to valid to all ways
 */
-/*
+reg [WAYS_W-1:0] hit_waynum;
+reg [1:0]       tlbway_command        [WAYS-1:0];
+wire [WAYS-1:0] tlbway_hit;
+wire [21:0]     tlbway_phys_r         [WAYS-1:0];
+wire [7:0]      tlbway_accesstag_r    [WAYS-1:0];
+
+integer i;
+always @* begin
+    hit_waynum = 0;
+    hit = tlbway_hit[0];
+    phys_r = tlbway_phys_r[0];
+    accesstag_r = tlbway_accesstag_r[0];
+
+    for(i = 0; i < WAYS; i = i + 1) begin
+        if(tlbway_hit[i]) begin
+            hit_waynum = i;
+            hit = tlbway_hit[hit_waynum];
+            phys_r = tlbway_phys_r[hit_waynum];
+            accesstag_r = tlbway_accesstag_r[hit_waynum];
+        end
+    end
+end
+
 genvar way_num;
 generate
 for(way_num = 0; way_num < WAYS; way_num = way_num + 1) begin : mem_generate_for
+    corevx_tlb_way #(ENTRIES_W, 0) u_tlb_way (
+        .clk(clk),
+        .rst_n(rst_n),
 
+        .command(tlbway_command[way_num]),
+
+        .virtual_address(virtual_address),
+        .hit(tlbway_hit[way_num]),
+        .accesstag_r(tlbway_accesstag_r[way_num]),
+        .phys_r(tlbway_phys_r[way_num]),
+
+        .virtual_address_w(virtual_address_w),
+        .accesstag_w(accesstag_w),
+        .phys_w(phys_w),
+
+        .invalidate_set_index(invalidate_set_index)
+    );
+
+    always @* begin
+        tlbway_command[way_num] = command;
+
+        if(command == `TLB_CMD_RESOLVE) begin
+            tlbway_command[way_num] = command;
+        end else if(command == `TLB_CMD_WRITE) begin
+            if(way_num == victim_way) begin
+                tlbway_command[way_num] = command;
+            end else begin
+                tlbway_command[way_num] = `TLB_CMD_NONE;
+            end
+        end
+    end
 end
 endgenerate
 
 endmodule
-*/
+
--- testbenches/tlb/corevx_tlb_tb.sv
+++ testbenches/tlb/corevx_tlb_tb.sv
@@ -1,6 +1,6 @@
 `timescale 1ns/1ns
 
-module tlb_testbench;
+module tlb_way_testbench;
 
 `include "../sync_clk_gen_template.svh"
 
@@ -11,7 +11,7 @@ end
 
 reg [1:0] command;
 // invalidate
-reg [ENTRIES_W:0] invalidate_set_index;
+reg [ENTRIES_W-1:0] invalidate_set_index;
 // write
 reg [7:0]  accesstag_w;
 reg [21:0] phys_w;
@@ -23,8 +23,9 @@ wire hit;
 wire [7:0] accesstag_r;
 wire [21:0] phys_r;
 
+localparam ENTRIES_W = 1;
 
-corevx_tlb tlb(
+corevx_tlb #(ENTRIES_W, 1, 0) tlb(
 	.*
 );
 
@@ -55,110 +56,109 @@ corevx_tlb tlb(
 
 initial begin
 	@(posedge rst_n)
-	@(negedge clk)
-	command
-	/*
-	@(posedge rst_n)
 
-	@(negedge clk) // 0 cycle begin
-			$display("Testing enable = 0");
-			invalidate = 0;
-			enable = 0;
-			write = 0;
-			resolve = 1;
-			virtual_address = 32'h0000_0000;
-	@(posedge clk)  // 0 cycle end
-		`assert(done, 0)
-		`assert(miss, 0)
-	@(posedge clk) // 1 cycle end
-		`assert(done, 1)
-		`assert(miss, 0)
-		$display("Testing enable = 0 done");
-	@(negedge clk) // 2 cycle begin
-		resolve = 0;
-	@(negedge clk) // 3 cycle begin
-		$display("Testing resolve to invalid");
-		enable = 1;
-		resolve = 1;
-	@(negedge clk) // 4 cycle begin
-		resolve = 0;
-	@(posedge clk) // 4 cycle end
-		`assert(done, 1)
-		`assert(miss, 1)
-		$display("Testing resolve to invalid done");
-	@(negedge clk) // 5 cycle begin
-		$display("Testing write");
-		write = 1;
-		virtual_address_w = 20'h2_0000;
-		phys_w = 22'h1_0000;
-		accesstag_w = 8'b1011_0001;
-	@(negedge clk) // 6 cycle begin
-		write = 1;
-		virtual_address_w = 20'h2_0001;
-		phys_w = 22'h1_0001;
-		accesstag_w = 8'b1011_0011;
-	@(negedge clk) // 7 cycle begin
-		write = 1;
-		virtual_address_w = 20'h2_0002;
-		phys_w = 22'h1_0002;
-		accesstag_w = 8'b1011_0101;
-	@(negedge clk) // 8 cycle begin
-		write = 0;
-		virtual_address = 20'h2_0000;
-		resolve = 1;
-	@(negedge clk) // 9 cycle begin
-		resolve = 0;
+	// invalidate all
+	@(negedge clk)
+	command <= `TLB_CMD_INVALIDATE;
+	invalidate_set_index <= 0;
 	@(posedge clk)
-		`assert(done, 1)
-		`assert(miss, 0)
-		`assert(accesstag_r, 8'b1011_0001);
-		`assert(phys_r, 20'h1_0000);
-		
-	@(negedge clk) // 10 cycle begin
-		virtual_address = 20'h2_0001;
-		enable = 1;
-		resolve = 1;
-	@(negedge clk) // 11 cycle begin
-		resolve = 0;
+	@(negedge clk)
+	invalidate_set_index <= 1;
+	@(posedge clk)
+
+	// tlb invalidate done
+
+	// tlb write 100 -> F5
+	@(negedge clk)
+
+	command <= `TLB_CMD_WRITE;
+	accesstag_w = 8'hFF;
+	phys_w <= 22'hF5;
+	virtual_address_w <= 20'h100;
+	@(negedge clk)
+
+	// tlb write 101 -> F5
+	command <= `TLB_CMD_WRITE;
+	accesstag_w = 8'hFF;
+	phys_w <= 22'hF5;
+	virtual_address_w <= 20'h101;
+	@(negedge clk)
+
+	// tlb write 55 -> FE
+	command <= `TLB_CMD_WRITE;
+	accesstag_w = 8'hFF;
+	phys_w <= 22'hFE;
+	virtual_address_w <= 20'h55;
+	@(negedge clk)
+
+	// tlb write 56 -> F5
+	command <= `TLB_CMD_WRITE;
+	accesstag_w = 8'hFF;
+	phys_w <= 22'hF5;
+	virtual_address_w <= 20'h56;
+	@(negedge clk)
 	
-	@(posedge clk) // 11 cycle end
-		`assert(done, 1)
-		`assert(miss, 0)
-		`assert(accesstag_r, 8'b1011_0011);
-		`assert(phys_r, 20'h1_0001);
-	@(negedge clk) // 12 cycle begin
-		virtual_address = 20'h2_0002;
-		enable = 1;
-		resolve = 1;
-	@(negedge clk) // 13 cycle begin
-		resolve = 0;
-	@(posedge clk) // 13 cycle end
-		`assert(done, 1)
-		`assert(miss, 0)
-		`assert(accesstag_r, 8'b1011_0101);
-		`assert(phys_r, 20'h1_0002);
-		$display("Testing write done");
-	@(negedge clk) // 14 cycle begin
-		$display("Testing invalidate");
-		invalidate = 1;
-	@(negedge clk) // 15 cycle begin
-		invalidate = 0;
-		virtual_address = 20'h2_0002;
-		enable = 1;
-		resolve = 1;
-	@(negedge clk) // 16 cycle begin
-		resolve = 0;
-	@(posedge clk) // 16 cycle end
-		`assert(done, 1)
-		`assert(miss, 1)
-	@(negedge clk) // 17 cycle begin
-		invalidate = 0;
-		enable = 0;
-		write = 0;
-		resolve = 0;
-		virtual_address = 32'h0000_0000;
-	$display("Testing done");
-	*/
+	// resolve test 55 -> FE
+	command <= `TLB_CMD_RESOLVE;
+	virtual_address <= 20'h55;
+
+	@(negedge clk)
+
+	`assert(hit, 1'b1);
+	`assert(accesstag_r, 8'hFF);
+	`assert(phys_r, 22'hFE);
+
+	// resolve test 56 -> F5
+	virtual_address <= 20'h56;
+
+	@(negedge clk)
+
+	`assert(hit, 1'b1);
+	`assert(accesstag_r, 8'hFF);
+	`assert(phys_r, 22'hF5);
+
+	// resolve test 100 -> F5
+	virtual_address <= 20'h100;
+
+	@(negedge clk)
+
+	`assert(hit, 1'b1);
+	`assert(accesstag_r, 8'hFF);
+	`assert(phys_r, 22'hF5);
+
+	// resolve test 101 -> F5
+	virtual_address <= 20'h101;
+
+	@(negedge clk)
+
+	`assert(hit, 1'b1);
+	`assert(accesstag_r, 8'hFF);
+	`assert(phys_r, 22'hF5);
+
+	// invalidate requests
+
+	command <= `TLB_CMD_INVALIDATE;
+	invalidate_set_index <= 0;
+	@(posedge clk)
+	@(negedge clk)
+	invalidate_set_index <= 1;
+	@(posedge clk)
+	@(negedge clk)
+	command <= `TLB_CMD_RESOLVE;
+	virtual_address <= 20'h55;
+	// test invalidation
+	@(negedge clk)
+	`assert(hit, 1'b0);
+	virtual_address <= 20'h56;
+	@(negedge clk)
+	`assert(hit, 1'b0);
+	virtual_address <= 20'h100;
+	@(negedge clk)
+	`assert(hit, 1'b0);
+	virtual_address <= 20'h101;
+	@(negedge clk)
+	`assert(hit, 1'b0);
+	$finish;
 end
 
 
