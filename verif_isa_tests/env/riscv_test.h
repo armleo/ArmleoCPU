696edcf31a6ca96ec0bcdfa55275a80e3382874e
armleo <aa-c-aa@mail.ru>
2020-05-20 10:06:38 +0400

    verif_isa_tests: Added basic verification isa tests infrastracture

---
+++ verif_isa_tests/env/riscv_test.h
@@ -0,0 +1,73 @@
+#define INIT_XREG                                                       \
+  li x1, 0;                                                             \
+  li x2, 0;                                                             \
+  li x3, 0;                                                             \
+  li x4, 0;                                                             \
+  li x5, 0;                                                             \
+  li x6, 0;                                                             \
+  li x7, 0;                                                             \
+  li x8, 0;                                                             \
+  li x9, 0;                                                             \
+  li x10, 0;                                                            \
+  li x11, 0;                                                            \
+  li x12, 0;                                                            \
+  li x13, 0;                                                            \
+  li x14, 0;                                                            \
+  li x15, 0;                                                            \
+  li x16, 0;                                                            \
+  li x17, 0;                                                            \
+  li x18, 0;                                                            \
+  li x19, 0;                                                            \
+  li x20, 0;                                                            \
+  li x21, 0;                                                            \
+  li x22, 0;                                                            \
+  li x23, 0;                                                            \
+  li x24, 0;                                                            \
+  li x25, 0;                                                            \
+  li x26, 0;                                                            \
+  li x27, 0;                                                            \
+  li x28, 0;                                                            \
+  li x29, 0;                                                            \
+  li x30, 0;                                                            \
+  li x31, 0;
+
+#define TESTNUM gp
+
+#define RVTEST_PASS                                                     \
+        fence;                                                          \
+        li a0, 0xD01E4A55;                                             \
+        sw a0, 0(x0);                                                   \
+        ebreak;
+
+#define RVTEST_FAIL                                                     \
+        fence;                                                          \
+        ebreak;
+
+
+#define RVTEST_RV32U
+#define RVTEST_CODE_BEGIN                                               \
+        .section .text.init;                                            \
+        .align  6;                                                      \
+        .weak stvec_handler;                                            \
+        .weak mtvec_handler;                                            \
+        .globl _start;                                                  \
+_start:                                                                 \
+        /* reset vector */                                              \
+        j reset_vector;                                                 \
+        .align 2;                                                       \
+trap_vector:                                                            \
+        RVTEST_FAIL                                                     \
+reset_vector:                                                           \
+
+
+#define RVTEST_CODE_END                                                 \
+        RVTEST_PASS
+
+#define RVTEST_DATA_BEGIN                                               \
+        .pushsection .tohost,"aw",@progbits;                            \
+        .align 6; .global tohost; tohost: .dword 0;                     \
+        .align 6; .global fromhost; fromhost: .dword 0;                 \
+        .popsection;                                                    \
+        .align 4; .global begin_signature; begin_signature:
+
+#define RVTEST_DATA_END .align 4; .global end_signature; end_signature:
