6c0631776c93efaecf78bdfa95286ba62f24e840
Arman Avetisyan <arman.avetisyan2000@gmail.com>
2020-09-16 11:24:09 +0400

    Testbench: First instruction checked

--- testbenches/decode/sim_main.cpp
+++ testbenches/decode/sim_main.cpp
@@ -45,15 +45,8 @@ void next_cycle() {
     armleocpu_decode->eval();
 }
 
-void check(bool match, string msg) {
-    
-    if(!match) {
-        cout << "testnum: " << dec << testnum << endl;
-        cout << msg << endl;
-        cout << flush;
-        throw runtime_error(msg);
-    }
-}
+#define check(expr) {if(!(expr)) {cout << "Testnum: " << testnum << " Simulation time:" << simulation_time << " Check failed: " #expr "" << endl; throw runtime_error("Failed: " #expr "");}}
+
 
 const uint32_t INSTR_NOP = 0b0010011;
 
@@ -61,6 +54,22 @@ const uint32_t E2D_CMD_NONE = 0;
 const uint32_t E2D_CMD_KILL = 1;
 const uint32_t E2D_CMD_FLUSH = 2;
 const uint32_t E2D_CMD_BRANCH = 3;
+
+const uint8_t MULDIV_OUTPUT_MUL = 0;
+const uint8_t MULDIV_OUTPUT_DEFAULT = MULDIV_OUTPUT_MUL;
+
+const uint8_t ALU_OUTPUT_ADD = 0;
+const uint32_t INSTR_ADD = 0b0110011;
+
+const uint8_t TYPE_ALU = 1;
+
+const uint8_t IN0_RS1 = 0;
+const uint8_t IN1_RS2 = 0;
+
+const uint8_t SHAMT_RS2 = 0;
+const uint8_t SHAMT_DEFAULT = SHAMT_RS2;
+
+const uint8_t RD_SEL_ALU = 0;
 
 void f2d_instr(uint8_t valid, uint32_t instr, uint32_t pc) {
     armleocpu_decode->f2d_instr_valid = valid;
@@ -76,6 +85,31 @@ void e2d_respond(uint8_t ready, uint8_t cmd, uint32_t jump_target, uint8_t write
     armleocpu_decode->e2d_rd_write = write;
     armleocpu_decode->e2d_rd_waddr = waddr;
     armleocpu_decode->eval();
+}
+
+
+void d2f_check(uint8_t ready, uint8_t cmd) {
+    check(armleocpu_decode->d2f_ready == ready);
+    check(armleocpu_decode->d2f_cmd == cmd);
+
+}
+
+
+void d2e_instr_invalid() {
+    check(armleocpu_decode->d2e_instr_valid == 0);
+}
+
+void d2e_alu_instr_check(uint32_t instr, uint8_t alu_select) {
+    check(armleocpu_decode->d2e_instr_valid == 1);
+    check(armleocpu_decode->d2e_instr_illegal == 0);
+    check(armleocpu_decode->d2e_instr_decode_alu_output_sel == alu_select);
+    check(armleocpu_decode->d2e_instr_decode_muldiv_sel == MULDIV_OUTPUT_DEFAULT);
+    check(armleocpu_decode->d2e_instr_decode_type == TYPE_ALU);
+    check(armleocpu_decode->d2e_instr_decode_alu_in0_mux_sel == IN0_RS1);
+    check(armleocpu_decode->d2e_instr_decode_alu_in1_mux_sel == IN1_RS2);
+    check(armleocpu_decode->d2e_instr_decode_shamt_sel == SHAMT_DEFAULT);
+    check(armleocpu_decode->d2e_instr_decode_rd_sel == RD_SEL_ALU);
+
 }
 
 int main(int argc, char** argv, char** env) {
@@ -119,31 +153,54 @@ int main(int argc, char** argv, char** env) {
         uint32_t pc;
 
         armleocpu_decode->rst_n = 1;
-
-        f2d_instr(0, INSTR_NOP, pc += 4);
+        armleocpu_decode->csr_mstatus_tsr = 0;
+        armleocpu_decode->csr_mstatus_tvm = 0;
+        armleocpu_decode->csr_mstatus_tw = 0;
+        armleocpu_decode->csr_mcurrent_privilege = 0b11;
+        // No instruction available
+        testnum = 0;
         e2d_respond(0, E2D_CMD_NONE, 0x1FF0, 0, 31);
-        
+        f2d_instr(0, INSTR_NOP, pc);
+        d2e_instr_invalid();
+        d2f_check(0, E2D_CMD_NONE);
         next_cycle();
+        
+        // Instruction available, execute waiting for instr
+        testnum = 1;
+        e2d_respond(0, E2D_CMD_NONE, 0x1FF0, 0, 31);
+        f2d_instr(1, INSTR_ADD, pc);
+        d2e_instr_invalid();
+        d2f_check(1, E2D_CMD_NONE);
+        next_cycle();
+        
 
-        d2f_ready == 0
-        d2f_cmd == E2D_CMD_NONE
-
-
-        d2e_instr_check();
-        d2e_instr_illegal == 0
-        d2e_instr_decode_alu_output_sel == ALU_OUTPUT_ADD
-        d2e_instr_decode_muldiv_sel == MULDIV_OUTPUT_DEFAULT
-        d2e_instr_decode_type == ALU
-        d2e_instr_decode_alu_in0_mux_sel == IN0_RS1
-        d2e_instr_decode_alu_in1_mux_sel == IN1_RS2
-        d2e_shamt_sel == SHAMT_DEFAULT
-        d2e_rd_sel == RD_SEL_ALU
-
+        testnum = 2;
+        e2d_respond(0, E2D_CMD_NONE, 0x1FF0, 0, 31);
         f2d_instr(1, INSTR_ADD, pc += 4);
-        e2d_respond(0, E2D_CMD_NONE, 0x1FF0, 0, 31);
+        d2e_alu_instr_check(INSTR_ADD, ALU_OUTPUT_ADD);
+        d2f_check(0, E2D_CMD_NONE);
         next_cycle();
 
+        testnum = 3;
+        e2d_respond(1, E2D_CMD_NONE, 0x1FF0, 0, 31);
+        f2d_instr(1, INSTR_ADD, pc);
+        d2e_alu_instr_check(INSTR_ADD, ALU_OUTPUT_ADD);
+        d2f_check(1, E2D_CMD_NONE);
+        next_cycle();
+
+        /*
+        d2e_alu_instr_check(INSTR, ALU_OUTPUT_ADD);
+        d2e_alui_instr_check(INSTR, ALU_OUTPUT_ADD);
+        d2e_muldiv_instr_check(INSTR, MULDIV_OUTPUT_MUL);
+        d2e_auipc_instr_check();
+        d2e_lui_instr_check();
         
+
+        rs1_read
+        rs1_addr
+        rs2_read
+        rs2_addr
+        */
         
         cout << "Decode tests done" << endl;
     } catch(exception e) {
