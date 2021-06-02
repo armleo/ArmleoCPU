6814320423f28f949ddcd53be45506f06a19ddfe
Armleo <arman.avetisyan2000@gmail.com>
2021-06-02 08:57:57 +0400

    Accesstag renamed to metadata

--- src/armleocpu_cache.v
+++ src/armleocpu_cache.v
@@ -2,53 +2,70 @@
 `include "armleocpu_defines.vh"
 
 module armleocpu_cache (
-    input                   clk,
-    input                   rst_n,
+    input wire              clk,
+    input wire              rst_n,
 
     //                      CACHE <-> EXECUTE/MEMORY
     /* verilator lint_off UNOPTFLAT */
     output reg   [3:0]      c_response, // CACHE_RESPONSE_*
     /* verilator lint_on UNOPTFLAT */
-    output wire             c_reset_done,
+    output reg              c_reset_done,
 
-    input  [3:0]            c_cmd, // CACHE_CMD_*
-    input  [31:0]           c_address,
-    input  [2:0]            c_load_type, // enum defined in ARMLEOCPU_defs LOAD_*
-    output wire  [31:0]     c_load_data,
-    input [1:0]             c_store_type, // enum defined in ARMLEOCPU_defs STORE_*
-    input [31:0]            c_store_data,
+    input wire [3:0]        c_cmd, // CACHE_CMD_*
+    input wire [31:0]       c_address,
+    input wire [2:0]        c_load_type, // enum defined in ARMLEOCPU_defs LOAD_*
+    output wire [31:0]      c_load_data,
+    input wire [1:0]        c_store_type, // enum defined in ARMLEOCPU_defs STORE_*
+    input wire [31:0]       c_store_data,
 
     //                      CACHE <-> CSR
-    //                      More in docs/
     //                      SATP from RISC-V privileged spec registered on FLUSH_ALL or SFENCE_VMA
-    input                   csr_satp_mode, // Mode = 0 -> physical access,
+    input wire              csr_satp_mode, // Mode = 0 -> physical access,
                                            // 1 -> ppn valid
-    input [21:0]            csr_satp_ppn,
+    input wire [21:0]       csr_satp_ppn,
     
-    //                      MPRV from RISC-V privileged spec
-    input                   csr_mstatus_mprv,
-    //                      MXR from RISC-V privileged spec
-    input                   csr_mstatus_mxr,
-    //                      SUM from RISC-V privileged spec
-    input                   csr_mstatus_sum,
-    //                      MPP from RISC-V privileged spec
-    input [1:0]             csr_mstatus_mpp,
+    //                      Signals from RISC-V privileged spec
+    input wire              csr_mstatus_mprv,
+    input wire              csr_mstatus_mxr,
+    input wire              csr_mstatus_sum,
+    input wire [1:0]        csr_mstatus_mpp,
 
-    //                      MPRV from armleocpu Extension
-    input [1:0]             csr_mcurrent_privilege,
+    // Current privilege level. Does not account for anything
+    input wire [1:0]        csr_mcurrent_privilege,
 
+    // AXI AW Bus
+    output reg          axi_awvalid,
+    input  wire         axi_awready,
+    output reg  [33:0]  axi_awaddr,
+    output reg          axi_awlock,
+    output wire [2:0]   axi_awprot,
 
+    // AXI W Bus
+    output reg          axi_wvalid,
+    input  wire         axi_wready,
+    output reg  [31:0]  axi_wdata,
+    output reg  [3:0]   axi_wstrb,
+    output reg          axi_wlast,
     
-    //                      CACHE <-> MEMORY
-    output reg              m_transaction,
-    output reg   [2:0]      m_cmd,         // enum `ARMLEOBUS_CMD_*
-    input                   m_transaction_done,
-    input        [2:0]      m_transaction_response, // enum `ARMLEOBUS_RESPONSE_*
-    output reg   [33:0]     m_address,
-    output reg   [3:0]      m_burstcount,
-    output reg   [31:0]     m_wdata,
-    output reg   [3:0]      m_wbyte_enable,
-    input        [31:0]     m_rdata
+    // AXI B Bus
+    input  wire         axi_bvalid,
+    output reg          axi_bready,
+    input  wire [1:0]   axi_bresp,
+    
+    output reg          axi_arvalid,
+    input  wire         axi_arready,
+    output reg  [33:0]  axi_araddr,
+    output reg  [7:0]   axi_arlen,
+    output reg  [1:0]   axi_arburst,
+    output reg          axi_arlock,
+    output wire [2:0]   axi_arprot,
+    
+
+    input wire          axi_rvalid,
+    output reg          axi_rready,
+    input wire  [1:0]   axi_rresp,
+    input wire          axi_rlast,
+    input wire  [31:0]  axi_rdata
 );
 
 
@@ -62,18 +79,17 @@ module armleocpu_cache (
 //`define DEBUG_CACHE_LANESTATE_READ
 
 
-
 parameter WAYS_W = 2;
 localparam WAYS = 2**WAYS_W;
 
 parameter TLB_ENTRIES_W = 4;
 parameter TLB_WAYS_W = 2;
 localparam TLB_ENTRIES = 2**TLB_ENTRIES_W;
-parameter BYPASS_ENABLED = 0;
-// TODO:
 
 parameter LANES_W = 1;
 localparam LANES = 2**LANES_W;
+
+parameter [0:0] IS_INSTURCTION_CACHE = 0;
 
 
 // 4 = 16 words each 32 bit = 64 byte
@@ -89,32 +105,30 @@ localparam VIRT_TAG_W = 20;
 
 
 
-
-
 // |------------------------------------------------|
 // |                                                |
 // |              Cache State                       |
 // |                                                |
 // |------------------------------------------------|
+`DEFINE_REG_REG_NXT(4, state, state_nxt, clk)
+`DEFINE_REG_REG_NXT(4, return_state, return_state_nxt, clk)
+`DEFINE_REG_REG_NXT(1, aborted, aborted_nxt, clk)
+`DEFINE_REG_REG_NXT(WAYS_W, victim_way, victim_way_nxt, clk)
 
-reg [3:0]   state;
-reg [3:0]   return_state;
-reg         aborted;
 localparam 	STATE_RESET = 4'd0,
             STATE_ACTIVE = 4'd1,
+            STATE_WRITE = 4'd2,
             STATE_REFILL = 4'd2,
             STATE_PTW = 4'd3;
-
-reg [WAYS_W-1:0] victim_way;
 
 // |------------------------------------------------|
 // |                                                |
 // |              Output stage                      |
 // |                                                |
 // |------------------------------------------------|
-reg                         os_active;
-reg                         os_error;
-reg                         os_error_type;
+`DEFINE_REG_REG_NXT(1, os_active, os_active_nxt, clk)
+`DEFINE_REG_REG_NXT(1, os_error, os_error_nxt, clk)
+`DEFINE_REG_REG_NXT(1, os_error_type, os_error_type_nxt, clk)
 
 //                          address decomposition
 reg [VIRT_TAG_W-1:0]        os_address_vtag; // Used by PTW
@@ -143,6 +157,7 @@ reg [21:0]                  csr_satp_ppn_r;
             `CACHE_CMD_STORE:       c_cmd_ascii = "STORE";
             `CACHE_CMD_FLUSH_ALL:   c_cmd_ascii = "FLUSH";
             `CACHE_CMD_ABORT:       c_cmd_ascii = "ABORT";
+            // TODO: Add commands
             default:                c_cmd_ascii = "UNKNOWN";
         endcase
     end
@@ -214,75 +229,6 @@ reg [21:0]                  csr_satp_ppn_r;
 // |                                                |
 // |------------------------------------------------|
 
-// PTAG port
-reg  [LANES_W-1:0]      ptag_lane;
-reg                     ptag_read           [WAYS-1:0];
-wire [CACHE_PHYS_TAG_W-1:0]
-                        ptag_readdata       [WAYS-1:0];
-reg                     ptag_write          [WAYS-1:0];
-reg  [PHYS_W-1:0]       ptag_writedata;
-
-//                      Storage read/write port vars
-reg  [LANES_W-1:0]      storage_lane    [WAYS-1:0];
-reg  [OFFSET_W-1:0]     storage_offset  [WAYS-1:0];
-reg  [WAYS-1:0]         storage_read;
-wire [31:0]             storage_readdata    [WAYS-1:0];
-reg  [WAYS-1:0]         storage_write;
-reg  [3:0]              storage_byteenable;
-reg  [31:0]             storage_writedata;
-
-//                      lanestate read port
-reg                     lanestate_read           [WAYS-1:0];
-reg  [LANES_W-1:0]      lanestate_lane           [WAYS-1:0];
-wire [WAYS-1:0]         lanestate_readdata       [1:0];
-//                      lanestate write port
-reg  [WAYS-1:0]         lanestate_write;
-reg  [1:0]              lanestate_writedata;
-
-
-// PTW request signals
-reg                     ptw_resolve_request;
-reg  [19:0]             ptw_resolve_vtag;
-// PTW result signals
-wire                    ptw_resolve_done;
-wire                    ptw_pagefault;
-wire                    ptw_accessfault;
-
-
-`ifdef DEBUG_PAGEFAULT
-wire [30*8-1:0] pagefault_reason;
-`endif
-
-wire [7:0]              ptw_resolve_access_bits;
-wire [PHYS_W-1:0]       ptw_resolve_phystag;
-
-// PTW AXI4 Signals
-
-// Store gen signals
-wire [31:0]             storegen_dataout;
-wire [3:0]              storegen_mask;
-wire                    storegen_missaligned;
-wire                    storegen_unknowntype;
-// Load gen signals
-reg [31:0]              loadgen_datain;
-wire                    loadgen_missaligned;
-wire                    loadgen_unknowntype;
-
-
-reg  [1:0]              tlb_command;
-
-// read port
-reg  [19:0]             tlb_resolve_virtual_address;
-wire                    tlb_hit;
-wire [7:0]              tlb_read_accesstag;
-wire [TLB_PHYS_TAG_W-1:0]       tlb_read_ptag;
-wire [CACHE_PHYS_TAG_W-1:0] tlb_read_cptag;
-
-// write port
-reg  [19:0]             tlb_write_vtag;
-reg  [7:0]              tlb_write_accesstag;
-reg  [TLB_PHYS_TAG_W-1:0]       tlb_write_ptag;
-
 
 // TODO: correctly handle vm_enabled csr_satp_mode_r
 wire [1:0] vm_privilege = ((csr_mcurrent_privilege == `ARMLEOCPU_PRIVILEGE_MACHINE) && csr_mstatus_mprv) ? csr_mstatus_mpp : csr_mcurrent_privilege;
@@ -306,8 +252,6 @@ reg                         missaligned;
 
 
 wire [PHYS_W-1:0]           cptag;
-[LANES_W-1:6-LANES_W-1]
-assign cptag = vm_enabled ? {tlb_read_ptag, } : {2'b00, os_address_vtag};
 
 // TODO: Calculate CPTAG
 
@@ -316,11 +260,35 @@ reg  [WAYS_W-1:0]           os_cache_hit_way;
 reg                         os_cache_hit;
 reg  [31:0]                 os_readdata;
 
-wire [WAYS-1:0]             os_valid_per_way = lanestate_readdata[0];
-wire [WAYS-1:0]             os_dirty_per_way = lanestate_readdata[1];
 
 
-assign c_reset_done = state != STATE_RESET;
+
+
+
+// PTAG port
+reg  [LANES_W-1:0]      cptag_lane;
+reg                     cptag_read;
+wire [CACHE_PHYS_TAG_W-1:0]
+                        cptag_readdata       [WAYS-1:0];
+reg                     cptag_write          [WAYS-1:0];
+reg  [PHYS_W-1:0]       cptag_writedata;
+
+//                      Storage read/write port vars
+reg  [LANES_W-1:0]      storage_lane;
+reg  [OFFSET_W-1:0]     storage_offset;
+reg  [WAYS-1:0]         storage_read;
+wire [31:0]             storage_readdata    [WAYS-1:0];
+reg  [WAYS-1:0]         storage_write;
+reg  [3:0]              storage_byteenable;
+reg  [31:0]             storage_writedata;
+
+//                      lanestate read port
+reg                     lanestate_read;
+reg  [LANES_W-1:0]      lanestate_lane;
+wire [WAYS-1:0]         lanestate_readdata;
+//                      lanestate write port
+reg  [WAYS-1:0]         lanestate_write;
+reg                     lanestate_writedata;
 
 
 genvar way_num;
@@ -332,25 +300,25 @@ for(way_num = 0; way_num < WAYS; way_num = way_num + 1) begin : mem_generate_for
         .WIDTH(CACHE_PHYS_TAG_W)
     ) ptag_storage (
         .clk(clk),
-        .address(ptag_lane[way_num]),
+        .address(cptag_lane),
 
-        .read(ptag_read[way_num]),
-        .readdata(ptag_readdata[way_num]),
+        .read(cptag_read),
+        .readdata(cptag_readdata[way_num]),
         
-        .write(ptag_write[way_num]),
-        .writedata(ptag_writedata)
+        .write(cptag_write[way_num]),
+        .writedata(cptag_writedata)
     );
     
     armleocpu_mem_1rw #(
         .ELEMENTS_W(LANES_W),
-        .WIDTH(2)
+        .WIDTH(1)
     ) lanestatestorage (
         .clk(clk),
         
-        .address(lanestate_lane[way_num]),
+        .address(lanestate_lane),
 
-        .read(lanestate_read[way_num]),
-        .readdata({lanestate_readdata[1][way_num], lanestate_readdata[0][way_num]}),
+        .read(lanestate_read),
+        .readdata(lanestate_readdata[way_num]),
 
         .write(lanestate_write[way_num]),
         .writedata(lanestate_writedata)
@@ -362,7 +330,7 @@ for(way_num = 0; way_num < WAYS; way_num = way_num + 1) begin : mem_generate_for
             .WIDTH(8)
         ) datastorage (
             .clk(clk),
-            .address({storage_lane[way_num], storage_offset[way_num]}),
+            .address({storage_lane, storage_offset}),
             
             .read(storage_read[way_num]),
             .readdata(storage_readdata[way_num][byte_offset+7:byte_offset]),
@@ -381,24 +349,33 @@ endgenerate
 // |                   LoadGen                      |
 // |------------------------------------------------|
 
+
+// Load gen signals
+reg [31:0]              loadgen_datain;
+wire                    loadgen_missaligned;
+wire                    loadgen_unknowntype;
 armleocpu_loadgen loadgen(
-    .inwordOffset       (os_address_inword_offset),
-    .loadType           (os_load_type),
+    .inword_offset          (os_address_inword_offset),
+    .loadgen_type           (os_load_type),
 
-    .LoadGenDataIn      (loadgen_datain),
+    .loadgen_datain         (loadgen_datain),
 
-    .LoadGenDataOut     (c_load_data),
-    .LoadMissaligned    (loadgen_missaligned),
-    .LoadUnknownType    (loadgen_unknowntype)
+    .loadgen_dataout        (c_load_data),
+    .loadgen_missaligned    (loadgen_missaligned),
+    .loadgen_unknowntype    (loadgen_unknowntype)
 );
 
 // |------------------------------------------------|
 // |                 StoreGen                       |
 // |------------------------------------------------|
 
-
+// Store gen signals
+wire [31:0]             storegen_dataout;
+wire [3:0]              storegen_mask;
+wire                    storegen_missaligned;
+wire                    storegen_unknowntype;
 armleocpu_storegen storegen(
-    .inwordOffset           (os_address_inword_offset),
+    .inword_offset          (os_address_inword_offset),
     .storegenType           (os_store_type),
 
     .storegenDataIn         (os_store_data),
@@ -410,34 +387,63 @@ armleocpu_storegen storegen(
 );
 
 
+
+// PTW request signals
+reg                     ptw_resolve_request;
+reg  [19:0]             ptw_resolve_virtual_address;
+// PTW result signals
+wire                    ptw_resolve_done;
+wire                    ptw_pagefault;
+wire                    ptw_accessfault;
+
+
+`ifdef DEBUG_PAGEFAULT
+wire [30*8-1:0] pagefault_reason;
+`endif
+
+wire [7:0]              ptw_resolve_metadata;
+wire [TLB_PHYS_TAG_W-1:0]       ptw_resolve_physical_address;
+// PTW AXI4 Signals
+
+
+
+
 // Page table walker instance
 armleocpu_ptw ptw(
     .clk                    (clk),
     .rst_n                  (rst_n),
 
-    .m_transaction          (ptw_m_transaction),
-    .m_cmd                  (ptw_m_cmd),
-    .m_address              (ptw_m_address),
-    .m_transaction_done     (m_transaction_done),
-    .m_transaction_response (m_transaction_response),
-    .m_rdata                (m_rdata),
+    // TODO: AXI Connection
     
     .resolve_request        (ptw_resolve_request),
-    /*verilator lint_off PINCONNECTEMPTY*/
-    .resolve_ack            (),
-    /*verilator lint_on PINCONNECTEMPTY*/
-    .virtual_address        (ptw_resolve_vtag/*os_address_vtag*/),
+    .virtual_address        (ptw_resolve_virtual_address/*os_address_vtag*/),
 
     .resolve_done           (ptw_resolve_done),
     .resolve_pagefault      (ptw_pagefault),
     .resolve_accessfault    (ptw_accessfault),
 
-    .resolve_access_bits    (ptw_resolve_access_bits),
-    .resolve_physical_address(ptw_resolve_phystag),
+    .ptw_resolve_metadata    (ptw_resolve_metadata),
+    .resolve_physical_address(ptw_resolve_physical_address),
 
-    .satp_mode              (csr_satp_mode_r),
     .satp_ppn               (csr_satp_ppn_r)
 );
+
+
+/*
+
+reg  [1:0]              tlb_command;
+
+// read port
+reg  [19:0]             tlb_resolve_virtual_address;
+wire                    tlb_hit;
+wire [7:0]              tlb_read_accesstag;
+wire [TLB_PHYS_TAG_W-1:0]       tlb_read_ptag;
+wire [CACHE_PHYS_TAG_W-1:0] tlb_read_cptag;
+
+// write port
+reg  [19:0]             tlb_write_vtag;
+reg  [7:0]              tlb_write_accesstag;
+reg  [TLB_PHYS_TAG_W-1:0]       tlb_write_ptag;
 
 armleocpu_tlb #(TLB_ENTRIES_W, TLB_WAYS_W) tlb(
     .rst_n                  (rst_n),
@@ -478,7 +484,10 @@ armleocpu_cache_pagefault pagefault_generator(
     `endif
 );
 
+[LANES_W-1:6-LANES_W-1]
+assign cptag = vm_enabled ? {tlb_read_ptag, } : {2'b00, os_address_vtag, };
 
+*/
 
 // |------------------------------------------------|
 // |         Output stage data multiplexer          |
@@ -501,8 +510,28 @@ always @* begin : output_stage_mux
 end
 
 
-// unknowntype, missaligned multiplexer
-always @* begin
+always @* begin : cache_comb
+    integer i;
+
+    axi_awvalid = 0;
+    axi_awaddr = ;
+    axi_awlock = 0;
+    axi_awprot = {IS_INSTURCTION_CACHE, vm_privilege > `ARMLEOCPU_PRIVILEGE_SUPERVISOR, vm_privilege > `ARMLEOCPU_PRIVILEGE_USER};
+    
+    axi_wvalid = 0;
+    axi_wdata = storegen_dataout;
+    axi_wstrb = storegen_mask;
+    axi_wlast = 0;
+
+    axi_bready = 0;
+
+    axi_arvalid = 0;
+    axi_araddr = ;
+    axi_arlen = ;
+    axi_arburst = ;
+    axi_arlock = 0;
+    axi_arprot = axi_awprot;
+
     if(os_cmd == `CACHE_CMD_LOAD) begin
         unknowntype = loadgen_unknowntype;
         missaligned = loadgen_missaligned;
@@ -510,41 +539,30 @@ always @* begin
         unknowntype = storegen_unknowntype;
         missaligned = storegen_missaligned;
     end
-end
 
-
-always @* begin : cache_comb
-    integer i;
 
     tlb_command = `TLB_CMD_NONE;
     stall = 1;
     c_response = `CACHE_RESPONSE_IDLE;
 
-    m_transaction = 0;
-    m_cmd = `ARMLEOBUS_CMD_NONE;
-    m_address = {ptag, os_address_lane, os_address_offset, 2'b00};
-    m_burstcount = 0;
-    m_wdata = storegen_dataout;
-    m_wbyte_enable = storegen_mask;
 
     for(i = 0; i < WAYS; i = i + 1) begin
-        ptag_read[i] = 1'b0;
-        ptag_readlane[i] = c_address_lane;
+        ptag_read = 1'b0;
+        ptag_readlane = c_address_lane;
         ptag_write[i] = 1'b0;
 
-        lanestate_read[i] = 1'b0;
-        lanestate_readlane[i] = c_address_lane;
+        lanestate_read = 1'b0;
+        lanestate_lane = c_address_lane;
         lanestate_write[i] = 1'b0;
 
-        storage_read[i] = 1'b0;
-        storage_readlane[i] = {LANES_W{1'b0}};
-        storage_readoffset[i] = {OFFSET_W{1'b0}};
+        storage_read = 1'b0;
+        storage_lane = refill_address_lane;
+        storage_offset = refill_address_offset;
         storage_write[i] = 1'b0;
+        storage_writedata = axi_rdata;
+        storage_byteenable = 1;
     end
-    storage_writedata = storegen_dataout;
-    storage_writelane = os_address_lane;
-    storage_writeoffset = os_address_offset;
-    storage_byteenable = storegen_mask;
+    
 
     lanestate_writelane = {LANES_W{1'b0}};
     lanestate_writedata = 2'b11; // valid and dirty
@@ -555,20 +573,34 @@ always @* begin : cache_comb
     ptw_resolve_vtag = os_address_vtag;
     loadgen_datain_sel = 0;
     
+    /*
     tlb_resolve_virtual_address = c_address_vtag;
     tlb_write_vtag = os_address_vtag;
     tlb_write_accesstag = ptw_resolve_access_bits;
     tlb_write_ptag = ptw_resolve_phystag;
     
-    if(tlb_invalidate_set_index == TLB_ENTRIES-1) begin
-        tlb_invalidate_done = 1'b1;
-    end else begin
-        tlb_invalidate_done = 1'b0;
-    end
     // used by state reset
     if(reset_lane_counter == LANES-1) begin
         reset_valid_reset_done = 1'b1;
     end else begin
         reset_valid_reset_done = 1'b0;
     end
+    */
+
+    c_reset_done = 0;
+
+    case (state)
+        STATE_RESET: begin
+            c_reset_done = 0;
+            for(i = 0; i < WAYS; i = i + 1)
+                lanestate_write[i] = 1'b1;
+            lanestate_writedata = 2'b00;
+            lanestate_writelane = reset_lane_counter;
+
+            tlb_command = `TLB_CMD_INVALIDATE;
+            
+            c_response = `CACHE_RESPONSE_WAIT;
+            stall = 1;
+        end
+
 end
