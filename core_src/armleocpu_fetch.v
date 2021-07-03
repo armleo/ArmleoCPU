9f11a93f665f64e08b62db8e3b1d7dd3adc20f4b
Arman Avetisyan <arman.avetisyan2000@gmail.com>
2021-07-03 20:47:45 +0400

    Fetch Unit: Work in progress

--- core_src/armleocpu_fetch.v
+++ core_src/armleocpu_fetch.v
@@ -29,7 +29,7 @@
 `TIMESCALE_DEFINE
 
 module armleocpu_fetch #(
-    parameter RESET_VECTOR = 32'h0000_2000;
+    parameter RESET_VECTOR = 32'h0000_2000
 ) (
     input                   clk,
     input                   rst_n,
@@ -63,452 +63,187 @@ module armleocpu_fetch #(
 
 );
 
+// Fetch unit
+// This unit sends fetch command to cache
+// It is required to keep command the same until cache responds
+// So there is three states:
+//      non active (no cmd issued in the past, next cmd can be issued)
+//      active and last request is done (next cmd can be issued)
+//      active and last request is not done ("stalled" or currently processing the command)
+// What we are doing is everytime we see non active or (active and last request is done)
+// conditions fetch send snext cmd depending on current command of E2F bus
+// Or we dont send any if E2F tells us to abort
 
-reg active;
+// This fetch was designed for 3 stage pipeline in mind.
+// As of currently slowest and highest delay element is cache response generation
+// So there is no purpose on registering E2F, so it is assumed that E2F will be directly connected
+// To decode unit.
 
-reg [31:0] pc;
+// Decode unit will abort operations as early as possible.
+// In some cases execute may cause interrupt or exception.
+// This means that decode stage will get branch taken and
+// same command will be issued to fetch
+// Fetch will abort its current fetch and will start fetching instruction
+// In location specified in the abort command.
 
-reg aborted;
-reg flush;
+// When current fetch is done or there is no active command request
+// and there was branch taken command then
+// next command will be executed from branch
+// If no branch is executed then continue fetching from PC + 4
 
-assign flushing = 
-        flush || (e2f_ready && e2f_cmd == `ARMLEOCPU_E2F_CMD_FLUSH);
+// Other edge case include pending interrupt
+// In case there is pending interrupt then according input signal will be set.
+// Then fetch unit will issue F2D with type == INTERRUPT PENDING
+// and will continue doing so until Execute unit will start interrupt handling
+// As interrupt handling starts xIE for current privilege level will be set to zero
+// causing interrupt pending signal to go low. Then Branch taken command will be issued
+// And fetch unit will start fetching from location of xTVEC which will be passed in branch target
 
-assign aborting =
+// For debugging dbg_mode input is used
+// When dbg_mode is set then when instruction fetch is done
+// next fetch command will not be issued. When no active command is sent,
+// then busy signal will go low
+
+// When Busy signal is low and dbg_mode is set then fetching is stopped
+// This allows debug unit to issue commands in dbg_cmd signal
+// Then dbg_cmd_busy will go high and until current command is done
+// It will not be deasserted.
+
+// Instructions that this unit will do are:
+// Set the PC: which will be treatede as "branch taken"
+// IFlush: Which will be treated as "flush command from pipeline"
+
+// As debug unit will not issue commands until all pipeline stages
+// will be in idle mode
+
+// TODO: What will happen to commands after debug mode is set
+// May be just register all commands? Only abort and branch taken can be issued
+// at the same time. This will mean that it can just accept all commands
+// from pipeline
+
+// TODO: What will happen if more than one E2F arrives with same command
+// Probably just use earlist one because the pipeline should have been reset anyway
+// So if it not then it's a BUG, so assert that this is not possible
+
+// Naming -ed and -ing.
+// -ed means that command was issued in the past
+// -ing means that command is active right now or somewhere in the past
+// aborted == abort command was recved in the past
+// aborting == there is current cmd - abort or abort recved while fetch was in progress
+
+
+`DEFINE_REG_REG_NXT(1, active, active_nxt, clk)
+`DEFINE_REG_REG_NXT(32, pc, pc_nxt, clk)
+
+`DEFINE_REG_REG_NXT(1, aborted, aborted_nxt, clk)
+`DEFINE_REG_REG_NXT(1, branched, branched_nxt, clk)
+`DEFINE_REG_REG_NXT(1, flushed, flushed_nxt, clk)
+
+wire flushing = 
+        flushed || (e2f_ready && e2f_cmd == `ARMLEOCPU_E2F_CMD_FLUSH);
+
+wire aborting =
         aborted || (e2f_ready && e2f_cmd == `ARMLEOCPU_E2F_CMD_ABORT);
 
-assign branching = 
+wire branching = 
         branched || (e2f_ready && e2f_cmd == `ARMLEOCPU_E2F_CMD_START_BRANCH);
-reg [31:0] branched_target = ;
 
-wire branching_target = (e2f_ready && e2f_cmd == `ARMLEOCPU_E2F_CMD_START_BRANCH) ? e2f_branchtarget : branched_target;
+`DEFINE_REG_REG_NXT(32, branched_target, branched_target_nxt, clk)
+
+wire [31:0] branching_target = (e2f_ready && e2f_cmd == `ARMLEOCPU_E2F_CMD_START_BRANCH) ? e2f_branchtarget : branched_target;
 
 //assign start_new_fetch = ((!active) || (active && c_done)) && !dbg_mode && (e2f_ready && e2f_cmd != `ARMLEOCPU_E2F_CMD_ABORT);
-
-always @* begin
-    if(!active) begin
-        if(dbg_mode || aborting) begin
-            // Dont start new fetch
-        end else if(flush) begin
-            // Issue flush
-        end else if(branching) begin
-            c_cmd = 
-            c_address = branching_target;
-        end else begin
-            // Can start new fetch at pc + 4
-            
-            c_address = pc + 4;
-        end
-    end else if(active && !c_done) begin
-        // Continue issuing whatever we were issuing
-        c_cmd = r_cmd;
-        c_address = pc;
-        busy = 1;
-    end else if(active && c_done) begin // no active request
-        c_cmd = next_cmd;
-        c_address = next_pc;
-        busy = 1;
-        
-        if(dbg_mode) begin
-            // Dont start new fetch
-        end else if(aborting) begin
-            
-        end else if(flush) begin
-
-        end else if(branching) begin
-
-        end else begin
-            // Can start new fetch at pc + 4
-        end
-    end
-
-
-    /*
-    else if() begin
-        if(interrupt_pending && active && c_done) begin
-            active_nxt = 0;
-            f2d_valid = 0;
-        end else if(dbg_mode && active && c_done) begin
-            active_nxt = 0;
-            f2d_valid = 0;
-        end else if((flush || (e2f_cmd == `ARMLEOCPU_E2F_CMD_FLUSH)) && active) begin
-            active_nxt = 0;
-            f2d_valid = 0;
-        end else if(interrupt_pending && !active) begin
-            f2d_valid = 1;
-            f2d_type = `F2E_TYPE_INTERRUPT_PENDING;
-            active_nxt = 0;
-        end else if(dbg_mode && !active) begin
-            f2d_valid = 0;
-            active_nxt = 0;
-        end else if((flush || (e2f_cmd == `ARMLEOCPU_E2F_CMD_FLUSH)) && active) begin
-            f2d_valid = 0;
-            active_nxt = 0;
-        end else if(flush && !active) begin
-            c_cmd = `CACHE_CMD_FLUSH_ALL;
-            active_nxt = 1;
-        end else begin
-            c_address = next_pc;
-            c_cmd = `CACHE_CMD_EXECUTE;
-            active_nxt = 1;
-            pc_nxt = next_pc;
-        end
-
-    if(c_done) begin
-        if(!flush) begin
-
-        end else if(!aborted) begin
-
-        end else begin
-
-        end
-    end
-
-    if(start_new_fetch) begin
-        c_address = pc_nxt;
-        if(flush) begin
-            c_cmd = `CACHE_CMD_FLUSH_ALL;
-        end else begin
-            c_cmd = `CACHE_CMD_EXECUTE;
-        end
-    end else begin
-        c_cmd = saved_cmd;
-        c_address = pc;
-    end*/
-end
-
-
-endmodule
-
-/*
-
-module armleocpu_fetch(
-    input                   clk,
-    input                   rst_n,
-
-    // IRQ/Exception Base address
-    input [31:0]            csr_mtvec,
-    input [31:0]            csr_stvec,
-
-    // CSR Registers
-    input [1:0]             csr_mcurrent_privilege,
-    input [15:0]            csr_medeleg,
-
-    // From debug
-    input                   dbg_request,
-    input                   dbg_set_pc,
-    input                   dbg_exit_request,
-    input [31:0]            dbg_pc,
-
-    // To Debug
-    output reg              dbg_mode,
-    // async signal:
-    output reg              dbg_done,
-
-
-
-
-
-    // Cache IF
-    input      [3:0]        c_response,
-    input                   c_reset_done,
-
-    output reg [3:0]        c_cmd,
-    output     [31:0]       c_address,
-    input      [31:0]       c_load_data,
-
-    // Interrupts
-    input                   interrupt_pending_csr,
-    input      [31:0]       interrupt_cause,
-    input      [31:0]       interrupt_target_pc,
-    input       [1:0]       interrupt_target_privilege,
-    
-    output reg              instret_incr,
-
-    // towards execute
-    output reg              f2e_instr_valid,
-    output reg [31:0]       f2e_instr,
-    output reg [31:0]       f2e_pc,
-    output reg              f2e_exc_start,
-    output reg [31:0]       f2e_epc,
-    output reg [31:0]       f2e_cause,
-    output reg  [1:0]       f2e_exc_privilege,
-
-    // from execute
-    input                                               e2f_ready,
-    input      [`ARMLEOCPU_E2F_CMD_WIDTH-1:0]           e2f_cmd,
-    input      [31:0]                                   e2f_bubble_jump_target,
-    input      [31:0]                                   e2f_branchtarget
-);
-
-parameter RESET_VECTOR = 32'h0000_2000;
-
-
-//STATE
-reg [31:0] pc;
-reg flushing;
-reg bubble;
-reg [31:0] saved_instr;
-
-//SIGNALS
-reg [31:0] pc_nxt;
-reg flushing_nxt;
-reg bubble_nxt;
-reg dbg_mode_nxt;
-reg f2e_exc_start_nxt;
-reg [31:0] f2e_cause_nxt;
-reg [31:0] f2e_epc_nxt;
-
-reg [1:0] f2e_exc_privilege_nxt;
-
-
-wire cache_done = c_response == `CACHE_RESPONSE_DONE;
-wire cache_error =  (c_response == `CACHE_RESPONSE_ACCESSFAULT) ||
-                    (c_response == `CACHE_RESPONSE_MISSALIGNED) ||
-                    (c_response == `CACHE_RESPONSE_PAGEFAULT);
-wire cache_idle =   (c_response == `CACHE_RESPONSE_IDLE);
-wire cache_wait =   (c_response == `CACHE_RESPONSE_WAIT);
-
-wire new_fetch_begin =
-                    (dbg_mode && dbg_exit_request && (cache_idle || cache_done)) ||
-                    (e2f_ready && (cache_done || cache_idle || cache_error));
 
 wire [31:0] pc_plus_4 = pc + 4;
 
 
-assign c_address = pc_nxt;
-
-always @(posedge clk)
-    flushing <= flushing_nxt;
-
-always @(posedge clk)
-    bubble <= bubble_nxt;
-
-always @(posedge clk)
-    pc <= pc_nxt;
-
-always @(posedge clk)
-    saved_instr <= f2e_instr;
-
-// reg dbg_mode;
-always @(posedge clk)
-    dbg_mode <= dbg_mode_nxt;
-always @(posedge clk)
-    f2e_cause <= f2e_cause_nxt;
-always @(posedge clk)
-    f2e_exc_start <= f2e_exc_start_nxt;
-always @(posedge clk)
-    f2e_exc_privilege <= f2e_exc_privilege_nxt;
-always @(posedge clk)
-    f2e_epc <= f2e_epc_nxt;
-
-
-// if dbg_mode ->
-//     output NOP
-// else if cache_wait -> NOP
-// else if cache_done ->
-//     if flushing -> NOP
-//     else -> output data from cache
-// else if idle ->
-//     if saved_valid -> output saved_instr
-//     else -> output NOP
-// else if error ->
-//     output NOP, start Exception
-
+// Fetch starts
+// Fetch ends, decode detects a CSR, sends ABORT, Fetch sees abort, does not start new fetch
+// Fetch starts from aborted location, no abort
 
 
 always @* begin
-    f2e_instr = c_load_data;
-    f2e_pc = pc;
-    f2e_instr_valid = 1;
-    if(!c_reset_done) begin
-        f2e_instr_valid = 0;
-    end else begin
-        // Output instr logic
-        if (dbg_mode) begin
-            // NOP
-            f2e_instr_valid = 0;
-        end else if(cache_wait) begin
-            // NOP
-            f2e_instr_valid = 0;
-        end else if(cache_done) begin
-            if(flushing) begin
-                // NOP
-                f2e_instr_valid = 0;
-            end else
-                f2e_instr = c_load_data;
-        end else if(cache_idle) begin
-            if(!bubble)
-                f2e_instr = saved_instr;
-            else begin
-                // NOP
-                f2e_instr_valid = 0;
-            end
-        end else if(cache_error) begin
-            // NOP
-            f2e_instr_valid = 0;
-        end
-        // TODO: Add check for else
-    end
-end
-
-
-// 
-// Command logic (not up to date)
-//     state:
-//         dbg_mode = 0, flushing = 0, bubble = 1, pc = reset_vector
-    
-//     if dbg_mode && !dbg_exit_request
-//         -> debug mode, handle debug commands;
-//         if dbg_set_pc then set bubble to 1
-//     else if flushing
-//         if(cache_done) ->
-//             send NOP
-//             set flushing to zero
-//         else ->
-//             send flush
-//     else if bubble && cache_idle
-//         start fetching from pc
-//         bubble = 0
-//     esle if new_fetch_begin
-//         if dbg_request ->
-//             dbg_mode = 1
-//         else if irq && irq_enabled ->
-//             bubble = 1
-//             pc_nxt = mtvec
-//             start_exception(INTERRUPT);
-//         else if e2f_exc_start
-//             bubble = 1
-//             pc_nxt = mtvec
-//         else if e2f_exc_mret
-//             bubble = 1
-//             pc_nxt = mepc
-//         else if e2f_exc_sret
-//             bubble = 1
-//             pc_nxt = sepc
-//         else if e2f_branchtaken
-//             pc_nxt = branchtarget
-//         else if e2f_flush
-//             bubble = 1
-//             pc_nxt = pc + 4
-//             cmd = flush
-//             flushing = 1
-//         else if cache_error
-//             buble = 1
-//             pc_nxt = mtvec
-//             start_exception(FETCH_ERROR)
-//         else
-//             pc_nxt = pc + 4
-//     else
-//         continue fetching from pc
-//     new_fetch_begin =   (dbg_mode && dbg_exit_request && (cache_idle || cache_done)) ||
-//                         (e2f_ready && (cache_done || cache_idle || cache_error)) ||
-    
-// 
-
-
-
-always @* begin
-    pc_nxt = pc;
-    bubble_nxt = bubble;
-    dbg_mode_nxt = dbg_mode;
     c_cmd = `CACHE_CMD_NONE;
-    f2e_exc_start_nxt = 1'b0;
-    f2e_cause_nxt = 0;
-    f2e_epc_nxt = 0;
-    f2e_exc_privilege_nxt = 0;
-    
-    flushing_nxt = flushing;
-    instret_incr = 0;
+    c_address = pc;
+    f2d_valid = 0;
+    f2d_type = `F2E_TYPE_INSTR;
+    f2d_instr = c_load_data;
+    f2d_pc = pc;
 
-    dbg_done = 0;
-    
-    
+    // Internal flip flops input signals
+    pc_nxt = pc;
+    aborted_nxt = aborted;
+    flush_nxt = flush;
+    branched_nxt = branched;
+    branched_target_nxt = branched_target;
+
     if(!rst_n) begin
-        bubble_nxt = 1;
-        flushing_nxt = 0;
-        dbg_mode_nxt = 0;
-        pc_nxt = RESET_VECTOR;
-    end else if(!c_reset_done) begin
-        
+        branched_target_nxt = RESET_VECTOR;
+        branched_nxt = 1;
+        aborted_nxt = 0;
+        flushed_nxt = 0;
+
+
+        pc_nxt = 0;
+        // This will be overwritten anyway, BUT it should be reseted anyway
+        // Just in case it's stuck in metastate
+
+        // Pretend that we accepted a branch by setting branched
+        // If branched is set and no instruction fetch is active
+        // Then it will continue execution from branch_target
     end else begin
-        if (dbg_mode && !dbg_exit_request) begin
-            dbg_done = cache_done;
-            if(dbg_set_pc) begin
-                pc_nxt = dbg_pc;
-                bubble_nxt = 1;
-                dbg_done = 1;
-            end
-        end else if (flushing) begin
-            if (cache_done) begin
-                // CMD = NONE
-                flushing_nxt = 0;
+        if(!active) begin
+            if(dbg_mode || aborting) begin
+                // Dont start new fetch
+            end else if(flushing) begin
+                // Issue flush
+            end else if(branching) begin
+                c_cmd = 
+                c_address = branching_target;
+                pc_nxt = branching_target;
             end else begin
-                c_cmd = `CACHE_CMD_FLUSH_ALL;
+                // Can start new fetch at pc + 4
+                c_address = pc + 4;
             end
-        end else if(bubble && cache_idle && e2f_ready) begin
-            c_cmd = `CACHE_CMD_EXECUTE;
-            pc_nxt = pc;
-            bubble_nxt = 0;
-            f2e_exc_start_nxt = 0;
-            f2e_cause_nxt = 0;
-            dbg_mode_nxt = 0;
-            f2e_exc_privilege_nxt = 0;
-        end else if (new_fetch_begin) begin
-            instret_incr = 1;
-            dbg_mode_nxt = 0;
-            if (dbg_request) begin
-                dbg_mode_nxt = 1;
-            end else if(interrupt_pending_csr) begin
-                bubble_nxt = 1;
-                pc_nxt = interrupt_target_pc;
-                f2e_exc_start_nxt = 1'b1;
-                f2e_epc_nxt = pc;
-                f2e_cause_nxt = interrupt_cause;
-                f2e_exc_privilege_nxt = interrupt_target_privilege;
-            end else if (e2f_cmd == `ARMLEOCPU_E2F_CMD_BUBBLE_JUMP) begin
-                bubble_nxt = 1;
-                pc_nxt = e2f_bubble_jump_target;
-            end else if (e2f_cmd == `ARMLEOCPU_E2F_CMD_FLUSH) begin
-                bubble_nxt = 1;
-                flushing_nxt = 1;
-                pc_nxt = pc_plus_4;
-            end else if (e2f_cmd == `ARMLEOCPU_E2F_CMD_BRANCHTAKEN) begin
-                pc_nxt = e2f_branchtarget;
-                c_cmd = `CACHE_CMD_EXECUTE;
-            end else if (cache_error) begin
-                bubble_nxt = 1;
-                f2e_epc_nxt = pc;
-                f2e_exc_start_nxt = 1'b1;
-                
-                if(c_response == `CACHE_RESPONSE_MISSALIGNED) begin
-                    f2e_cause_nxt = `EXCEPTION_CODE_INSTRUCTION_ADDRESS_MISSALIGNED;
-                end else if(c_response == `CACHE_RESPONSE_ACCESSFAULT) begin
-                    f2e_cause_nxt = `EXCEPTION_CODE_INSTRUCTION_ACCESS_FAULT;
-                end else if(c_response == `CACHE_RESPONSE_PAGEFAULT) begin
-                    f2e_cause_nxt = `EXCEPTION_CODE_INSTRUCTION_PAGE_FAULT;
-                end
-                
-                if((csr_mcurrent_privilege != `ARMLEOCPU_PRIVILEGE_MACHINE) && csr_medeleg[f2e_cause_nxt]) begin
-                    f2e_exc_privilege_nxt =  `ARMLEOCPU_PRIVILEGE_SUPERVISOR;
-                    pc_nxt = csr_stvec;
-                end else begin
-                    f2e_exc_privilege_nxt =  `ARMLEOCPU_PRIVILEGE_MACHINE;
-                    pc_nxt = csr_mtvec;
-                end
+        end else if(active && !c_done) begin
+            // Continue issuing whatever we were issuing
+            c_cmd = r_cmd;
+            c_address = pc;
+            busy = 1;
+
+            // Remember all E2F's
+            // TODO: Assert that no E2Fs will get overwritten
+        end else if(active && c_done) begin // no active request
+            c_cmd = next_cmd;
+            c_address = next_pc;
+            busy = 1;
+            
+            if(aborting) begin
+                f2d_valid = 0; // Request that was aborted
+            end else if(flushing) begin
+                flush_nxt = 0;
+            end else if() begin
+
+            end
+
+            if(dbg_mode || aborting) begin
+                // Dont start new fetch
+            end else if(flush) begin
+                // Issue flush
+            end else if(branching) begin
+                c_cmd = 
+                c_address = branching_target;
             end else begin
-                pc_nxt = pc_plus_4;
-                c_cmd = `CACHE_CMD_EXECUTE;
+                // Can start new fetch at pc + 4
+
+                c_address = pc + 4;
             end
-        end else if (e2f_ready) begin
-            pc_nxt = pc;
-            c_cmd = `CACHE_CMD_EXECUTE;
         end
     end
 end
+
 
 endmodule
-*/
 
 
 `include "armleocpu_undef.vh"
