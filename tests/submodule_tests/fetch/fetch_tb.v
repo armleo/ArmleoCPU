04d4f56657f54d58e68d4027586242bd4d0e3019
Armleo <7474152+armleo@users.noreply.github.com>
2021-07-04 23:43:20 +0400

    Fetch: Work in progress

---
+++ tests/submodule_tests/fetch/fetch_tb.v
@@ -0,0 +1,41 @@
+////////////////////////////////////////////////////////////////////////////////
+// 
+// This file is part of ArmleoCPU.
+// ArmleoCPU is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// ArmleoCPU is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with ArmleoCPU.  If not, see <https://www.gnu.org/licenses/>.
+// 
+// Copyright (C) 2016-2021, Arman Avetisyan, see COPYING file or LICENSE file
+// SPDX-License-Identifier: GPL-3.0-or-later
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+`define TIMEOUT 100
+`define SYNC_RST
+`define CLK_HALF_PERIOD 5
+
+`define MAXIMUM_ERRORS 1
+
+`include "template.vh"
+
+initial begin
+
+    @(posedge rst_n)
+
+    $display("Testbench: Starting fetch testing");
+
+    $display("Testbench: Tests passed");
+    $finish;
+end
+
+
+endmodule
