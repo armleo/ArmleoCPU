6e37f55868486276055c8e007054b1209a4d63ea
Arman Avetisyan <arman.avetisyan2000@gmail.com>
2021-07-11 21:14:09 +0400

    CSR: First clean build

--- core_src/armleocpu_csr.v
+++ core_src/armleocpu_csr.v
@@ -87,7 +87,7 @@ module armleocpu_csr(
     //      Example: SRET is executed. CSR_CMD is signaled and csr_next_pc is registered
     //      Problem arises in case that for example execute relies on csr_mcurrent_privilege
     //      As on second cycle this is now set to value of PP causing pagefault
-    //      Which is an issue. Instead Execute should raise a flag
+    //      Which is an issue. Instead Execute implementation should raise a flag
     //      When this flag is raised then execute just does e2d command
     //      Ignoring all CSR outputs
 
@@ -99,7 +99,7 @@ module armleocpu_csr(
     output reg [31:0]   csr_next_pc,
 
     input      [11:0]   csr_address, // 12 bit address
-    output reg          csr_invalid,
+    output wire         csr_invalid,
     // Shows that Invalid instruction should be generated
 
     output reg [31:0]   csr_to_rd, // Towards register
@@ -126,7 +126,13 @@ wire  csr_read =  csr_cmd == `ARMLEOCPU_CSR_CMD_READ ||
 
 `ifdef FORMAL_RULES
 always @(posedge clk) begin
-    assert(csr_read || csr_write || (csr_cmd == `ARMLEOCPU_CSR_CMD_NONE));
+    assert(
+        csr_read || csr_write
+        || (csr_cmd == `ARMLEOCPU_CSR_CMD_NONE)
+        || (csr_cmd == `ARMLEOCPU_CSR_CMD_INTERRUPT_BEGIN)
+        || (csr_cmd == `ARMLEOCPU_CSR_CMD_MRET)
+        || (csr_cmd == `ARMLEOCPU_CSR_CMD_SRET)
+    );
 end
 `endif
 
@@ -134,7 +140,7 @@ wire accesslevel_invalid = (csr_write || csr_read) && (csr_mcurrent_privilege < 
 wire write_invalid = (csr_write && (csr_address[11:10] == 2'b11));
 reg csr_exists;
 
-wire csr_invalid = accesslevel_invalid | write_invalid | csr_exists;
+assign csr_invalid = (csr_read || csr_write) && (accesslevel_invalid | write_invalid | !csr_exists);
 
 // holds read modify write operations first operand,
 // because for mip, sip value used for RMW sequence is
@@ -147,8 +153,8 @@ reg [31:0] rmw_after;
 
 
 // TODO: Maybe make it REG instead of OREG because it's not connected to output
-`DEFINE_CSR_OREG(32, csr_mtvec, 0)
-`DEFINE_CSR_OREG(32, csr_stvec, 0)
+`DEFINE_CSR_REG(32, csr_mtvec, 0)
+`DEFINE_CSR_REG(32, csr_stvec, 0)
 
 `DEFINE_CSR_OREG(2, csr_mcurrent_privilege, 2'b11)
 `DEFINE_CSR_OREG(1, csr_mstatus_tsr, 0)
@@ -255,7 +261,7 @@ wire [31:0] csr_misa = {
 // This signal is calculated SIE
 // It is calculated because this signal is low
 // To block supervisor level interrupts in machine mode
-reg supervisor_user_calculated_sie;
+//reg supervisor_user_calculated_sie;
 
 
 always @* begin
@@ -283,7 +289,7 @@ reg irq_meip_en;
 reg irq_msip_en;
 reg irq_ssip_en;
 
-reg 
+//reg 
 
 always @* begin
 
@@ -370,6 +376,7 @@ always @* begin
     //  if machine just disable
 
     // TODO: Check logic below
+    /*
     supervisor_user_calculated_sie = 0;
     machine_calculated_mie =
         (
@@ -393,6 +400,7 @@ always @* begin
     irq_calculated_stie = supervisor_user_calculated_sie & csr_mie_stie;
     irq_calculated_ssie = supervisor_user_calculated_sie & csr_mie_ssie;
 
+    
     // TODO: Fix calculated IP signals
     irq_calculated_meip = irq_meip_i | csr_mip_meip;
     irq_calculated_mtip = irq_mtip_i; // No according mtip signal
@@ -407,7 +415,7 @@ always @* begin
             (csr_mideleg_external_interrupt ? csr_mip_seip || irq_exti_i : irq_exti_i);
     swi_pending = irq_swi_en & 
             (csr_mideleg_software_interrupt ? csr_mip_ssip || irq_swi_i : irq_swi_i);
-
+    */
 
     if(csr_cmd == `ARMLEOCPU_CSR_CMD_INTERRUPT_BEGIN) begin
 
@@ -417,6 +425,248 @@ always @* begin
 
     end else if(csr_write || csr_read) begin
 
+        case(csr_address)
+            `DEFINE_CSR_COMB_RO(12'hF11, MVENDORID)
+            `DEFINE_CSR_COMB_RO(12'hF12, MARCHID)
+            `DEFINE_CSR_COMB_RO(12'hF13, MIMPID)
+            `DEFINE_CSR_COMB_RO(12'hF14, MHARTID)
+            12'hFC0: begin // MCURRENT_PRIVILEGE
+                // This is used only for debug purposes
+                csr_exists = 1;
+                csr_to_rd = {30'h0, csr_mcurrent_privilege};
+                rmw_before = csr_to_rd;
+                if(!csr_invalid && csr_write) begin
+                    csr_mcurrent_privilege_nxt = rmw_after[1:0];
+                end
+            end
+            12'h300: begin // MSTATUS
+                csr_exists = 1;
+                csr_to_rd = {
+                            9'h0, // Padding SD, 8 empty bits
+                            csr_mstatus_tsr, csr_mstatus_tw, csr_mstatus_tvm, // trap enable bits
+                            csr_mstatus_mxr, csr_mstatus_sum, csr_mstatus_mprv, //machine privilege mode
+                            2'b00, 2'b00, // xs, fs
+                            csr_mstatus_mpp, 2'b00, csr_mstatus_spp, // MPP, 2 bits (reserved by spec), SPP
+                            csr_mstatus_mpie, 1'b0, csr_mstatus_spie, 1'b0,
+                            csr_mstatus_mie, 1'b0, csr_mstatus_sie, 1'b0};
+                rmw_before = csr_to_rd;
+                if(!csr_invalid && csr_write) begin
+                    csr_mstatus_tsr_nxt = rmw_after[22];
+                    csr_mstatus_tw_nxt = rmw_after[21];
+                    csr_mstatus_tvm_nxt = rmw_after[20];
+                    csr_mstatus_mxr_nxt = rmw_after[19];
+                    csr_mstatus_sum_nxt = rmw_after[18];
+                    csr_mstatus_mprv_nxt = rmw_after[17];
+
+                    if(rmw_after[12:11] != 2'b10)
+                        csr_mstatus_mpp_nxt = rmw_after[12:11];
+                    csr_mstatus_spp_nxt = rmw_after[8];
+
+                    csr_mstatus_mpie_nxt = rmw_after[7];
+                    csr_mstatus_spie_nxt = rmw_after[5];
+                    csr_mstatus_mie_nxt = rmw_after[3];
+                    csr_mstatus_sie_nxt = rmw_after[1];
+                end
+            end
+            `DEFINE_CSR_COMB_RO(12'h301, csr_misa)
+
+            // TODO: Address CSR definition contains error.
+            // Value should be written for all valid bits, and invalids are ignored
+            // Also note that *TVEC CSR may change in future
+            // Because 2 bottom bits are used to select interrupt scheme
+
+            `DEFINE_ADDRESS_CSR_REG_COMB(12'h305, csr_mtvec)
+            `DEFINE_SCRATCH_CSR_REG_COMB(12'h340, csr_mscratch)
+            `DEFINE_ADDRESS_CSR_REG_COMB(12'h341, csr_mepc)
+            `DEFINE_SCRATCH_CSR_REG_COMB(12'h342, csr_mcause)
+            `DEFINE_SCRATCH_CSR_REG_COMB(12'h343, csr_mtval)
+            
+            // Supervisor
+            `DEFINE_ADDRESS_CSR_REG_COMB(12'h105, csr_stvec)
+            `DEFINE_SCRATCH_CSR_REG_COMB(12'h140, csr_sscratch)
+            `DEFINE_ADDRESS_CSR_REG_COMB(12'h141, csr_sepc)
+            `DEFINE_SCRATCH_CSR_REG_COMB(12'h142, csr_scause)
+            `DEFINE_SCRATCH_CSR_REG_COMB(12'h143, csr_stval)
+
+
+            `DEFINE_SCRATCH_CSR_REG_COMB(12'hB00, csr_cycle)
+            `DEFINE_SCRATCH_CSR_REG_COMB(12'hB80, csr_cycleh)
+
+            `DEFINE_SCRATCH_CSR_REG_COMB(12'hB02, csr_instret)
+            `DEFINE_SCRATCH_CSR_REG_COMB(12'hB82, csr_instreth)
+            12'h180: begin // SATP
+                csr_to_rd = {csr_satp_mode, 9'h0, csr_satp_ppn};
+                csr_exists = !(csr_mstatus_tvm && csr_mcurrent_privilege == `ARMLEOCPU_PRIVILEGE_SUPERVISOR);
+                rmw_before = csr_to_rd;
+                if(!csr_invalid && csr_write) begin
+                    csr_satp_mode_nxt = rmw_after[31];
+                    csr_satp_ppn_nxt = rmw_after[21:0];
+                end
+            end
+            // MEDELEG
+            `DEFINE_CSR_COMB_RO(12'h302, 0)
+            // MIDELEG
+            `DEFINE_CSR_COMB_RO(12'h303, 0)
+
+            12'h304: begin // MIE
+                csr_invalid = accesslevel_invalid;
+
+                csr_to_rd = 0;
+
+                csr_to_rd[11] = csr_mie_meie;
+                csr_to_rd [9] = csr_mie_seie;
+
+                csr_to_rd [7] = csr_mie_mtie;
+                csr_to_rd [5] = csr_mie_stie;
+
+                csr_to_rd [3] = csr_mie_msie;
+                csr_to_rd [1] = csr_mie_ssie;
+
+                rmw_before = csr_to_rd;
+                if(csr_write && !csr_invalid) begin
+                    csr_mie_meie_nxt = rmw_after[11];
+                    csr_mie_seie_nxt = rmw_after [9];
+                    csr_mie_mtie_nxt = rmw_after [7];
+                    csr_mie_stie_nxt = rmw_after [5];
+                    csr_mie_msie_nxt = rmw_after [3];
+                    csr_mie_ssie_nxt = rmw_after [1];
+                end
+            end
+            12'h104: begin // SIE
+                csr_invalid = accesslevel_invalid;
+                csr_to_rd = 0;
+                csr_to_rd [9] = csr_mie_seie;
+
+                csr_to_rd [5] = csr_mie_stie;
+
+                csr_to_rd [1] = csr_mie_ssie;
+
+                rmw_before = csr_to_rd;
+                if(!csr_invalid && csr_write) begin
+                    csr_mie_seie_nxt = rmw_after [9];
+                    csr_mie_stie_nxt = rmw_after [5];
+                    csr_mie_ssie_nxt = rmw_after [1];
+                end
+            end
+            12'h100: begin // SSTATUS
+                csr_invalid = accesslevel_invalid;
+                csr_to_rd = {
+                            9'h0, // Padding SD, 9 empty bits
+                            3'b000, // trap enable bits
+                            csr_mstatus_mxr, csr_mstatus_sum, 1'b0, //mxr, sum, mprv
+                            2'b00, 2'b00, // xs, fs
+                            2'b00, 2'b00, csr_mstatus_spp, // MPP, 2 bits (reserved by spec), SPP
+                            1'b0, 1'b0, csr_mstatus_spie, 1'b0,
+                            1'b0, 1'b0, csr_mstatus_sie, 1'b0};
+                rmw_before = csr_to_rd;
+                if(!csr_invalid && csr_write) begin
+                    csr_mstatus_mxr_nxt = rmw_after[19];
+                    csr_mstatus_sum_nxt = rmw_after[18];
+                    csr_mstatus_spp_nxt = rmw_after[8];
+                    csr_mstatus_spie_nxt = rmw_after[5];
+                    csr_mstatus_sie_nxt = rmw_after[1];
+                end
+            end
+            // TODO: Fix all of below code:
+            /*
+            12'h344: begin // MIP
+                csr_invalid = accesslevel_invalid;
+                rmw_before = 0;
+
+                // EXTI
+                // 11th bit, read only
+                rmw_before[11] = irq_exti_i;
+
+                // s*ip bit, read/write if mideleg, else zero
+                // when read seip is logical or of this bit and external signal
+                // when rmw then seip is this bit
+                if(csr_mideleg_external_interrupt)
+                    rmw_before[9] = csr_mip_seip;
+                
+
+                // TIMER
+                rmw_before[7] = irq_timer_i;
+                
+                if(csr_mideleg_timer_interrupt)
+                    rmw_before[5] = csr_mip_stip;
+                
+                // SWI
+                rmw_before[3] = irq_swi_i;
+                
+                if(csr_mideleg_software_interrupt)
+                    rmw_before[1] = csr_mip_ssip;
+
+
+                csr_to_rd = rmw_before;
+
+                // s*ip bit, read/write if mideleg, else zero
+                // when read seip is logical or of this bit and external signal
+                // when rmw then seip is this bit
+                if(csr_mideleg_external_interrupt)
+                    csr_to_rd[9] = csr_mip_seip || irq_exti_i;
+                if(csr_mideleg_timer_interrupt)
+                    csr_to_rd[5] = csr_mip_stip || irq_timer_i;
+                if(csr_mideleg_software_interrupt)
+                    csr_to_rd[1] = csr_mip_ssip || irq_swi_i;
+                
+
+                if(!csr_invalid && csr_write) begin
+                    // csr_mip_m*ip is read only
+
+                    if(csr_mideleg_external_interrupt)
+                        csr_mip_seip_nxt = rmw_after[9];
+                    if(csr_mideleg_timer_interrupt)
+                        csr_mip_stip_nxt = rmw_after[5];
+                    if(csr_mideleg_software_interrupt)
+                        csr_mip_ssip_nxt = rmw_after[1];
+                end
+            end
+            12'h144: begin // SIP
+                csr_invalid = accesslevel_invalid;
+                rmw_before = 0;
+
+                // s*ip bit, read/write if mideleg, else zero
+                // when read seip is logical or of this bit and external signal
+                // when rmw then seip is this bit
+                if(csr_mideleg_external_interrupt)
+                    rmw_before[9] = csr_mip_seip;
+                
+                if(csr_mideleg_timer_interrupt)
+                    rmw_before[5] = csr_mip_stip;
+                
+                if(csr_mideleg_software_interrupt)
+                    rmw_before[1] = csr_mip_ssip;
+
+
+                csr_to_rd = rmw_before;
+
+                // s*ip bit, read/write if mideleg, else zero
+                // when read seip is logical or of this bit and external signal
+                // when rmw then seip is this bit
+                if(csr_mideleg_external_interrupt)
+                    csr_to_rd[9] = csr_mip_seip || irq_exti_i;
+                if(csr_mideleg_timer_interrupt)
+                    csr_to_rd[5] = csr_mip_stip || irq_timer_i;
+                if(csr_mideleg_software_interrupt)
+                    csr_to_rd[1] = csr_mip_ssip || irq_swi_i;
+                
+                if(!csr_invalid && csr_write) begin
+                    // csr_mip_m*ip is read only
+
+                    if(csr_mideleg_external_interrupt)
+                        csr_mip_seip_nxt = rmw_after[9];
+                    if(csr_mideleg_timer_interrupt)
+                        csr_mip_stip_nxt = rmw_after[5];
+                    if(csr_mideleg_software_interrupt)
+                        csr_mip_ssip_nxt = rmw_after[1];
+                end
+            end
+            */
+
+            default: begin
+                csr_exists = 0;
+            end
+        endcase
     end
 
 
--- tests/submodule_tests/csr/sim_main.cpp
+++ tests/submodule_tests/csr/sim_main.cpp
@@ -44,6 +44,8 @@ const int USER = 0;
 
 uint32_t testnum;
 
+bool error_happened;
+
 using namespace std;
 
 double sc_time_stamp() {
@@ -103,7 +105,7 @@ void csr_read(uint32_t address) {
 void csr_write_nocheck(uint32_t address, uint32_t data) {
     armleocpu_csr->csr_cmd = ARMLEOCPU_CSR_CMD_WRITE;
     armleocpu_csr->csr_address = address;
-    armleocpu_csr->csr_writedata = data;
+    armleocpu_csr->csr_from_rs = data;
     armleocpu_csr->eval();
 }
 
@@ -115,11 +117,11 @@ void csr_write(uint32_t address, uint32_t data) {
 
 void csr_read_check(uint32_t val) {
     armleocpu_csr->eval();
-    if(armleocpu_csr->csr_readdata != val)
-        cout << "Unexpected csr_readdata for address: 0x" << hex << armleocpu_csr->csr_address
-        << ", value is 0x" << armleocpu_csr->csr_readdata
+    if(armleocpu_csr->csr_to_rd != val)
+        cout << "Unexpected csr_to_rd for address: 0x" << hex << armleocpu_csr->csr_address
+        << ", value is 0x" << armleocpu_csr->csr_to_rd
         << ", expected: 0x" << val << endl << dec;
-    check(armleocpu_csr->csr_readdata == val, "Unexpected readdata value");
+    check(armleocpu_csr->csr_to_rd == val, "Unexpected readdata value");
 }
 
 void test_mro(uint32_t address, uint32_t expected_value) {
@@ -145,17 +147,21 @@ void csr_none() {
 }
 
 void test_scratch(uint32_t address) {
+    cout << "Testbench: test_scratch: Writing all ones to scratch" << endl;
     csr_write(address, 0xFFFFFFFF);
     next_cycle();
 
+    cout << "Testbench: test_scratch: reading all ones" << endl;
     csr_read(address);
     
     csr_read_check(0xFFFFFFFF);
     next_cycle();
 
+    cout << "Testbench: test_scratch: writing zero" << endl;
     csr_write(address, 0);
     next_cycle();
 
+    cout << "Testbench: test_scratch: Reading all zero" << endl;
     csr_read(address);
     csr_read_check(0);
     next_cycle();
@@ -163,18 +169,19 @@ void test_scratch(uint32_t address) {
     csr_none();
     next_cycle();
 
+    cout << "Testbench: test_scratch: Reading after dummy cycle" << endl;
     csr_read(address);
     csr_read_check(0);
     next_cycle();
 
+
+    // TODO: Add writing with dummy cycle
     csr_none();
 }
 
 
 void force_to_machine() {
-    armleocpu_csr->csr_exc_privilege = MACHINE;
-    armleocpu_csr->csr_exc_cause = 18;
-    armleocpu_csr->csr_exc_epc = 0xF204;
+    armleocpu_csr->irq_meip_i = 1;
     armleocpu_csr->csr_cmd = ARMLEOCPU_CSR_CMD_INTERRUPT_BEGIN;
     next_cycle();
 
@@ -190,7 +197,7 @@ void from_machine_go_to_privilege(uint32_t target_privilege) {
     csr_none();
     check(armleocpu_csr->csr_mcurrent_privilege == target_privilege, "GOTOPRIVILEGE: Unexpected target privilege");
 }
-
+/*
 // Does not do dummy cycle
 void interrupt_test(uint32_t from_privilege, uint32_t mstatus, uint32_t mideleg, uint32_t mie,
         bool irq_exti_i, bool irq_swi_i, bool irq_timer_i,
@@ -237,7 +244,7 @@ void interrupt_test(uint32_t from_privilege, uint32_t mstatus, uint32_t mideleg,
     else
         throw "Unexpected 'expected_privilege' value";
     
-}
+}*/
 
 
 int main(int argc, char** argv, char** env) {
@@ -275,9 +282,14 @@ int main(int argc, char** argv, char** env) {
     armleocpu_csr->rst_n = 0;
     armleocpu_csr->csr_cmd = ARMLEOCPU_CSR_CMD_NONE;
     armleocpu_csr->instret_incr = 0;
-    armleocpu_csr->irq_timer_i = 0;
-    armleocpu_csr->irq_exti_i = 0;
-    armleocpu_csr->irq_swi_i = 0;
+
+
+    armleocpu_csr->irq_mtip_i = 0;
+    armleocpu_csr->irq_meip_i = 0;
+    armleocpu_csr->irq_seip_i = 0;
+    armleocpu_csr->irq_msip_i = 0;
+    armleocpu_csr->irq_ssip_i = 0;
+
     csr_none();
     next_cycle();
 
@@ -448,7 +460,7 @@ int main(int argc, char** argv, char** env) {
 
     testnum = 18;
     csr_read(0xB00);
-    uint32_t begin_value = armleocpu_csr->csr_readdata;
+    uint32_t begin_value = armleocpu_csr->csr_to_rd;
     cout << "Testing MCYCLE: Start time = " << begin_value << endl;
     next_cycle();
     
@@ -532,25 +544,27 @@ int main(int argc, char** argv, char** env) {
     check(armleocpu_csr->csr_satp_ppn == 0x3FFFFF, "unexpected satp ppn");
     next_cycle();
 
-
+    // TODO: Fix this. This should be zero
     testnum = 33;
     cout << "Testing MEDELEG" << endl;
-    csr_write(0x302, 0xFFFF);
+    csr_write(0x302, 0xFFFFFFFF);
     next_cycle();
 
     csr_read(0x302);
-    csr_read_check(0xBBFF);
-    check(armleocpu_csr->csr_medeleg == 0xBBFF, "MEDELEG csr_medeleg output is incorrect");
+    csr_read_check(0);
     next_cycle();
 
     testnum = 33;
     cout << "Testing MIDELEG" << endl;
-    csr_write(0x303, 0xFFFF);
+    csr_write(0x303, 0xFFFFFFFF);
     next_cycle();
 
     csr_read(0x303);
-    csr_read_check(0x222);
+    csr_read_check(0);
     next_cycle();
+
+
+
 
     testnum = 34;
     cout << "Testing MIE" << endl;
@@ -615,139 +629,140 @@ int main(int argc, char** argv, char** env) {
     csr_write(0x300, 0b1000); // mstatus.mie
     next_cycle();
 
-
-    #define TEST_MIP(irq_input_signal, bit_shift) \
-    testnum++;\
-    csr_write(0x303, 0);/*mideleg*/ \
-    next_cycle(); \
-    \
-    csr_write(0x304, 1 << (bit_shift + MACHINE)); /*mie*/\
-    next_cycle(); \
-    \
-    irq_input_signal = 1; \
-    csr_none(); \
-    next_cycle(); \
-    \
-    csr_read(0x344); \
-    csr_read_check(1 << (bit_shift + MACHINE)); \
-    next_cycle(); \
-    \
-    csr_read(0x144); \
-    csr_read_check(0); \
-    next_cycle(); \
-    testnum++;\
-    irq_input_signal = 0; \
-    csr_none(); \
-    next_cycle(); \
-    \
-    csr_write(0x303, 1 << (bit_shift + SUPERVISOR)); /*mideleg*/\
-    next_cycle(); \
-    \
-    csr_write(0x304, 1 << (bit_shift + SUPERVISOR)); /*sie*/\
-    next_cycle(); \
-    \
-    irq_input_signal = 1; \
-    csr_none(); \
-    next_cycle(); \
- \
-    csr_read(0x344); \
-    csr_read_check((1 << (bit_shift + MACHINE)) | (1 << (bit_shift + SUPERVISOR))); \
-    next_cycle(); \
-\
-    csr_read(0x144); \
-    csr_read_check((1 << (bit_shift + SUPERVISOR))); \
-    next_cycle(); \
- \
-    irq_input_signal = 0; \
-    csr_none(); \
-    next_cycle(); \
-
-    TEST_MIP(armleocpu_csr->irq_exti_i, 8)
-    TEST_MIP(armleocpu_csr->irq_timer_i, 4)
-    TEST_MIP(armleocpu_csr->irq_swi_i, 0)
     
-    {   
-        testnum = 100;
-        uint32_t mie = 3;
-        uint32_t sie = 1;
 
-        uint32_t meie = 11;
-        uint32_t seie = 9;
+//     #define TEST_MIP(irq_input_signal, bit_shift) \
+//     testnum++;\
+//     csr_write(0x303, 0); /*mideleg*/ \
+//     next_cycle(); \
+//     \
+//     csr_write(0x304, 1 << (bit_shift + MACHINE)); /*mie*/\
+//     next_cycle(); \
+//     \
+//     irq_input_signal = 1; \
+//     csr_none(); \
+//     next_cycle(); \
+//     \
+//     csr_read(0x344); \
+//     csr_read_check(1 << (bit_shift + MACHINE)); \
+//     next_cycle(); \
+//     \
+//     csr_read(0x144); \
+//     csr_read_check(0); \
+//     next_cycle(); \
+//     testnum++;\
+//     irq_input_signal = 0; \
+//     csr_none(); \
+//     next_cycle(); \
+//     \
+//     csr_write(0x303, 1 << (bit_shift + SUPERVISOR)); /*mideleg*/\
+//     next_cycle(); \
+//     \
+//     csr_write(0x304, 1 << (bit_shift + SUPERVISOR)); /*sie*/\
+//     next_cycle(); \
+//     \
+//     irq_input_signal = 1; \
+//     csr_none(); \
+//     next_cycle(); \
+//  \
+//     csr_read(0x344); \
+//     csr_read_check((1 << (bit_shift + MACHINE)) | (1 << (bit_shift + SUPERVISOR))); \
+//     next_cycle(); \
+// \
+//     csr_read(0x144); \
+//     csr_read_check((1 << (bit_shift + SUPERVISOR))); \
+//     next_cycle(); \
+//  \
+//     irq_input_signal = 0; \
+//     csr_none(); \
+//     next_cycle(); \
 
-        uint32_t mtie = 7;
-        uint32_t stie = 5;
+//     TEST_MIP(armleocpu_csr->irq_exti_i, 8)
+//     TEST_MIP(armleocpu_csr->irq_timer_i, 4)
+//     TEST_MIP(armleocpu_csr->irq_swi_i, 0)
+    
+//     {   
+//         testnum = 100;
+//         uint32_t mie = 3;
+//         uint32_t sie = 1;
 
-        uint32_t msie = 3;
-        uint32_t ssie = 1;
+//         uint32_t meie = 11;
+//         uint32_t seie = 9;
+
+//         uint32_t mtie = 7;
+//         uint32_t stie = 5;
+
+//         uint32_t msie = 3;
+//         uint32_t ssie = 1;
 
 
 
 
-        std::vector<int> privlist = {MACHINE, SUPERVISOR, USER};
-        std::vector<int> supervisor_privlist = {SUPERVISOR, USER};
+//         std::vector<int> privlist = {MACHINE, SUPERVISOR, USER};
+//         std::vector<int> supervisor_privlist = {SUPERVISOR, USER};
 
-        for(auto & priv : privlist) {
-            interrupt_test(priv,
-                1 << mie, // mstatus.mie = 1
-                0, // mideleg
-                (1 << meie) | (1 << mtie) | (1 << msie), // mie
-                1, // exti
-                1, // swi
-                1, // timeri
-                (1 << 31) | (11), // cause
-                MACHINE // EXPECTED PRIVILEGE
-            );
-            testnum++;
-        }
+//         for(auto & priv : privlist) {
+//             interrupt_test(priv,
+//                 1 << mie, // mstatus.mie = 1
+//                 0, // mideleg
+//                 (1 << meie) | (1 << mtie) | (1 << msie), // mie
+//                 1, // exti
+//                 1, // swi
+//                 1, // timeri
+//                 (1 << 31) | (11), // cause
+//                 MACHINE // EXPECTED PRIVILEGE
+//             );
+//             testnum++;
+//         }
         
-        for(auto & priv : supervisor_privlist) {
-            interrupt_test(priv,
-                1 << sie, // mstatus.mie = 0, mstatus.sie = 1
-                (1 << seie) | (1 << stie) | (1 << ssie), // mideleg
-                (1 << seie) | (1 << stie) | (1 << ssie), // mie
-                1, // exti
-                1, // swi
-                1, // timeri
-                (1 << 31) | (11), // cause
-                SUPERVISOR // EXPECTED PRIVILEGE
-            );
-            testnum++;
-        }
+//         for(auto & priv : supervisor_privlist) {
+//             interrupt_test(priv,
+//                 1 << sie, // mstatus.mie = 0, mstatus.sie = 1
+//                 (1 << seie) | (1 << stie) | (1 << ssie), // mideleg
+//                 (1 << seie) | (1 << stie) | (1 << ssie), // mie
+//                 1, // exti
+//                 1, // swi
+//                 1, // timeri
+//                 (1 << 31) | (11), // cause
+//                 SUPERVISOR // EXPECTED PRIVILEGE
+//             );
+//             testnum++;
+//         }
 
-        // TODO: Test sie not set
-        // TODO: Test s*ie not set
-        // TODO: priority tests
-        // TODO: Test other interrupts
+//         // TODO: Test sie not set
+//         // TODO: Test s*ie not set
+//         // TODO: priority tests
+//         // TODO: Test other interrupts
 
-        // TODO: Test exception
-        // TODO: ARMLEOCPU_CSR_CMD_INTERRUPT_BEGIN
-    }
-    testnum = 200;
-    std::vector<int> privlist = {MACHINE, SUPERVISOR, USER};
-    for(auto & priv : privlist) {
-        force_to_machine();
-        uint32_t mpp = priv;
-        uint32_t mpie = 1;
-        uint32_t mstatus = (mpp << 11) | (mpie << 7);
-        uint32_t mepc = 0xF000C400;
-        csr_write(0x300, mstatus);
-        next_cycle();
-
-
-        csr_write(0x341, mepc);
-        next_cycle();
+//         // TODO: Test exception
+//         // TODO: ARMLEOCPU_CSR_CMD_INTERRUPT_BEGIN
+//     }
+//     testnum = 200;
+//     std::vector<int> privlist = {MACHINE, SUPERVISOR, USER};
+//     for(auto & priv : privlist) {
+//         force_to_machine();
+//         uint32_t mpp = priv;
+//         uint32_t mpie = 1;
+//         uint32_t mstatus = (mpp << 11) | (mpie << 7);
+//         uint32_t mepc = 0xF000C400;
+//         csr_write(0x300, mstatus);
+//         next_cycle();
 
 
-        armleocpu_csr->csr_cmd = ARMLEOCPU_CSR_CMD_MRET;
-        armleocpu_csr->eval();
-        check(armleocpu_csr->csr_next_pc == mepc, "mret: csr_next_pc is not mepc");
-        next_cycle();
+//         csr_write(0x341, mepc);
+//         next_cycle();
+
+
+//         armleocpu_csr->csr_cmd = ARMLEOCPU_CSR_CMD_MRET;
+//         armleocpu_csr->eval();
+//         check(armleocpu_csr->csr_next_pc == mepc, "mret: csr_next_pc is not mepc");
+//         next_cycle();
         
-        check(armleocpu_csr->csr_mcurrent_privilege == mpp, "mret: incorrect privilege");
-        csr_none();
+//         check(armleocpu_csr->csr_mcurrent_privilege == mpp, "mret: incorrect privilege");
+//         csr_none();
 
-        testnum++;
-    }
+//         testnum++;
+//     }
 
     // TODO: Test with rmw sequence
     // TODO: Test with privilege change the MIP's
@@ -774,7 +789,7 @@ int main(int argc, char** argv, char** env) {
         cout << e.what() << endl;
         next_cycle();
         next_cycle();
-        
+        error_happened;
     }
     armleocpu_csr->final();
     if (m_trace) {
@@ -790,5 +805,7 @@ int main(int argc, char** argv, char** env) {
     delete armleocpu_csr; armleocpu_csr = NULL;
 
     // Fin
+    if(error_happened)
+        exit(-1);
     exit(0);
 }
