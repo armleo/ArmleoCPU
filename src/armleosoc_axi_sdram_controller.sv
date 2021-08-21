558ebb565f9f49007319237fdebcc44322975590
Armleo <7474152+armleo@users.noreply.github.com>
2021-08-21 23:42:38 +0400

    FetchTB: WIP new architecture

---
+++ src/armleosoc_axi_sdram_controller.sv
@@ -0,0 +1,4 @@
+
+// Here should be SDRAM Controller implementation
+// But this implementation uses shared ADDR and DATA signals with multiple CS# signals
+// This implementation is also compatible with io_share_unit
