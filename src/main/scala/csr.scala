3e6d77aefb9cc0c74670d63f0616f3dc504b412c
Armleo <7474152+armleo@users.noreply.github.com>
2022-11-22 20:17:53 +0400

    CSR: More registers and shorthand proc made

--- src/main/scala/csr.scala
+++ src/main/scala/csr.scala
@@ -61,30 +61,22 @@ object csr_cmd extends ChiselEnum {
   val none, write, read, read_write, read_set, read_clear, interrupt, exception, mret, sret = Value
 }
 
+class exc_code(c: coreParams) extends ChiselEnum{
+  val INTERRUPT = (1.U << c.xLen - 1)
+
+
+  val MACHINE_SOFTWATE_INTERRUPT = ((1.U) | INTERRUPT)
+  val SUPERVISOR_SOFTWATE_INTERRUPT = ((3.U) | INTERRUPT)
+
+  val MACHINE_TIMER_INTERRUPT = ((5.U) | INTERRUPT)
+  val SUPERVISOR_TIMER_INTERRUPT = ((7.U) | INTERRUPT)
+
+  val MACHINE_EXTERNAL_INTERRUPT = ((9.U)| INTERRUPT)
+  val SUPERVISOR_EXTERNAL_INTERRUPT = ((11.U) | INTERRUPT)
+}
 
 class CSR(c: coreParams) extends Module {
-  object int_code {
-    val SUPERVISOR_SOFTWATE_INTERRUPT = (1.U)
-    val MACHINE_SOFTWATE_INTERRUPT = (3.U)
-    val SUPERVISOR_TIMER_INTERRUPT = (5.U)
-    val MACHINE_TIMER_INTERRUPT = (7.U)
-    val SUPERVISOR_EXTERNAL_INTERRUPT = (9.U)
-    val MACHINE_EXTERNAL_INTERRUPT = (11.U)
-  }
-
-  object exc_code {
-    val INTERRUPT = (1.U << c.xLen)
-
-
-    val MACHINE_SOFTWATE_INTERRUPT = (int_code.MACHINE_SOFTWATE_INTERRUPT | INTERRUPT)
-    val SUPERVISOR_SOFTWATE_INTERRUPT = (int_code.SUPERVISOR_SOFTWATE_INTERRUPT | INTERRUPT)
-
-    val MACHINE_TIMER_INTERRUPT = (int_code.MACHINE_TIMER_INTERRUPT | INTERRUPT)
-    val SUPERVISOR_TIMER_INTERRUPT = (int_code.SUPERVISOR_TIMER_INTERRUPT | INTERRUPT)
-
-    val MACHINE_EXTERNAL_INTERRUPT = (int_code.MACHINE_EXTERNAL_INTERRUPT | INTERRUPT)
-    val SUPERVISOR_EXTERNAL_INTERRUPT = (int_code.SUPERVISOR_EXTERNAL_INTERRUPT | INTERRUPT)
-  }
+  
 
 
   /**************************************************************************/
@@ -161,13 +153,13 @@ class CSR(c: coreParams) extends Module {
   val mscratch            = Reg(UInt(c.xLen.W))
   val sscratch            = Reg(UInt(c.xLen.W))
 
-  val mepc                = Reg(SInt(c.xLen.W))
-  val sepc                = Reg(SInt(c.xLen.W))
+  val mepc                = Reg(UInt(c.xLen.W))
+  val sepc                = Reg(UInt(c.xLen.W))
 
-  val mcause              = Reg(SInt(c.xLen.W))
-  val scause              = Reg(SInt(c.xLen.W))
+  val mcause              = Reg(UInt(c.xLen.W))
+  val scause              = Reg(UInt(c.xLen.W))
 
-  val stval               = Reg(SInt(c.xLen.W))
+  val stval               = Reg(UInt(c.xLen.W))
   
   val cycle_counter       = RegInit(0.U(c.xLen.W))
       cycle_counter      := cycle_counter + 1.U
@@ -289,27 +281,72 @@ class CSR(c: coreParams) extends Module {
         (calculated_stip & calculated_stie) |
         (calculated_ssip & calculated_ssie);
 
-  exc_int_error := true.B
 
+  def ro(ro_addr: UInt, value: UInt): Unit = {
+    when(addr === ro_addr) {
+      exists := true.B
+      out := value
+
+    }
+  }
+
+  def scratch(a: UInt, r: UInt): Unit = {
+    when(addr === a) {
+      exists := true.B
+      out := r
+      rmw_before := r
+      when(!invalid && write) {
+        r := rmw_after
+      }
+    }
+  }
+
+  def partial(a: UInt, top: Int, bot: Int, w: UInt, r: UInt): Unit = {
+    when(addr === a) {
+      exists := true.B
+      out := w
+      rmw_before := w
+      when(!invalid && write) {
+        r := rmw_after(top, bot)
+      }
+    }
+  }
+
+  def addr_reg(a: UInt, r: UInt): Unit = {
+    when(addr === a) {
+      exists := true.B
+      out := r
+      rmw_before := r
+      when(!invalid && write) {
+        // TODO: Is this an okay requirement?
+        r := rmw_after & ~(3.U(c.xLen))
+      }
+    }
+  }
+
+  
+  exc_int_error := true.B
+  next_pc := mtvec
+  out := 0.U
   when(cmd === csr_cmd.interrupt) {
     // Note: Order matters, checkout the interrupt priority in RISC-V Privileged Manual
     when( calculated_meip &  calculated_meie) { // MEI
-        mcause := exc_code.MACHINE_EXTERNAL_INTERRUPT; // Calculated by the CSR
+        mcause := new exc_code(c).MACHINE_EXTERNAL_INTERRUPT; // Calculated by the CSR
         exc_int_error := false.B
     } .elsewhen( calculated_msip &  calculated_msie) { // MSI
-        mcause := exc_code.MACHINE_SOFTWATE_INTERRUPT; // Calculated by the CSR
+        mcause := new exc_code(c).MACHINE_SOFTWATE_INTERRUPT; // Calculated by the CSR
         exc_int_error := false.B
     } .elsewhen( calculated_mtip &  calculated_mtie) { // MTI
-        mcause := exc_code.MACHINE_TIMER_INTERRUPT; // Calculated by the CSR
+        mcause := new exc_code(c).MACHINE_TIMER_INTERRUPT; // Calculated by the CSR
         exc_int_error := false.B
     } .elsewhen( calculated_seip &  calculated_seie) { // SEI
-        mcause := exc_code.SUPERVISOR_EXTERNAL_INTERRUPT; // Calculated by the CSR
+        mcause := new exc_code(c).SUPERVISOR_EXTERNAL_INTERRUPT; // Calculated by the CSR
         exc_int_error := false.B
     } .elsewhen( calculated_ssip &  calculated_ssie) { // SSI
-        mcause := exc_code.SUPERVISOR_SOFTWATE_INTERRUPT; // Calculated by the CSR
+        mcause := new exc_code(c).SUPERVISOR_SOFTWATE_INTERRUPT; // Calculated by the CSR
         exc_int_error := false.B
     } .elsewhen( calculated_stip &  calculated_stie) { // STI
-        mcause := exc_code.SUPERVISOR_TIMER_INTERRUPT; // Calculated by the CSR
+        mcause := new exc_code(c).SUPERVISOR_TIMER_INTERRUPT; // Calculated by the CSR
         exc_int_error := false.B
     } .otherwise {
         exc_int_error := true.B
@@ -358,8 +395,87 @@ class CSR(c: coreParams) extends Module {
     exc_int_error := false.B
   } .elsewhen (read || write) {
     exc_int_error := false.B
+    
+    ro      ("hF11".U, c.mvendorid.U)
+    ro      ("hF12".U, c.marchid.U)
+    ro      ("hF13".U, c.mimpid.U)
+    ro      ("hF14".U, c.mhartid.U)
+    ro      ("hF15".U, c.mconfigptr.U)
+    
+
+
+    val mstatus = Cat(
+      "h0".U(9.W), // Padding SD, 8 empty bits
+      hyptrap.tsr, hyptrap.tw, hyptrap.tvm, // trap enable bits
+      mem_priv.mxr, mem_priv.sum, mem_priv.mprv, //machine privilege mode
+      "b00"U(2.W), "b00"U(2.W), // xs, fs
+      mem_priv.mpp, "b00"U(2.W), spp, // MPP, 2 bits (reserved by spec), SPP
+      mpie, "b0"U(1.W), spie, "b0"U(1.W),
+      mie, "b0"U(1.W), sie, "b0"U(1.W)
+    )
+    partial ("h300".U, 1, 1,    mstatus,          sie)
+    partial ("h300".U, 3, 3,    mstatus,          mie)
+    partial ("h300".U, 5, 5,    mstatus,          spie)
+    partial ("h300".U, 7, 7,    mstatus,          mpie)
+    partial ("h300".U, 8, 8,    mstatus,          spp)
+    partial ("h300".U, 12, 11,  mstatus, mem_priv.mpp)
+    partial ("h300".U, 17, 17,  mstatus, mem_priv.mprv)
+    partial ("h300".U, 18, 18,  mstatus, mem_priv.sum)
+    partial ("h300".U, 19, 19,  mstatus, mem_priv.mxr)
+    partial ("h300".U, 20, 20,  mstatus, hyptrap .tvm)
+    partial ("h300".U, 21, 21,  mstatus, hyptrap .tw)
+    partial ("h300".U, 22, 22,  mstatus, hyptrap .tsr)
+
+    // MSTATUSH
+    // Added in v1.12 of privileged spec
+    ro      ("h310".U, 0.U)
+
+    // TODO: F ISA writable
+    ro      ("h301".U, isa)
+
+    ro      ("h306".U, 0.U) // mcounteren
+    ro      ("h106".U, 0.U) // scounteren
+
+    addr_reg("h305".U, mtvec)
+    scratch ("h340".U, mscratch)
+    addr_reg("h341".U, mepc)
+    scratch ("h340".U, mcause)
+    ro      ("h343".U, 0.U) // MTVAL is hardwired to zero, in case it never gets written
+    
+    // Supervisor
+    addr_reg("h105".U, stvec)
+    scratch ("h140".U, sscratch)
+    addr_reg("h141".U, sepc)
+    scratch ("h142".U, scause)
+    scratch ("h143".U, stval)
+    // STVAL is NOT hardwired to zero
+    // because it needs to be written by M-level bootloader
+    // To pass the interrupt/exceptions
+
+    
+    if(c.xLen == 32) {
+      scratch ("hB00".U, cycle_counter)
+      scratch ("hB80".U, cycle_counter(63, 32))
+      scratch ("hB00".U, instret_counter)
+      scratch ("hB82".U, instret_counter(63, 32))
+    } else {
+      scratch ("hB00".U, cycle_counter)
+      scratch ("hB00".U, instret_counter)
+    }
+
+    ro      ("h302".U, 0.U) // MEDELEG
+    ro      ("h303".U, 0.U) // MIDELEG
+
+    // TODO: Add SATP
   }
   
   rmw_before := 0.U
   exists := false.B
 }
+
+import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
+
+object CSRGenerator extends App {
+  (new ChiselStage).execute(Array("--target-dir", "generated_vlog"), Seq(ChiselGeneratorAnnotation(() => new CSR(new coreParams()))))
+}
+
